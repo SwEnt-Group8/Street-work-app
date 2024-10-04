4be2d6ec21ae9211cc4bdcfe156d120f597475f2
tercierp <paul.tercier@epfl.ch>
2024-10-04 18:08:41 +0200

    fix(tests): simplify MainActivity by removing the setContent

--- app/src/main/java/com/android/streetworkapp/MainActivity.kt
+++ app/src/main/java/com/android/streetworkapp/MainActivity.kt
@@ -2,11 +2,9 @@ package com.android.streetworkapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {}
   }
 }
