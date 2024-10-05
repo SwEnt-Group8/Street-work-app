24a5e23c6d82040bfbffcb8f88e5a2ae44551c48
Alvaro Moya <alvaromoya@macbook-air-de-alvaro-2.home>
2024-10-05 23:07:39 +0200

    fix: setup for the use of OkHttp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ fun DependencyHandlerScope.globalTestImplementation(dep: Any) {
 }
 
 dependencies {
+    implementation(libs.okhttp)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
