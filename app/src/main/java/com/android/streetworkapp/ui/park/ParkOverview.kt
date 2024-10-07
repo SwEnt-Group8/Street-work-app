c9268e72fa10b5f460c47b5185db2ac1d3786e96
Arthur Muster <arthur@must-r.com>
2024-10-07 17:16:10 +0200

    feat(ui): implement EventItemList composable

--- app/src/main/java/com/android/streetworkapp/ui/park/ParkOverview.kt
+++ app/src/main/java/com/android/streetworkapp/ui/park/ParkOverview.kt
@@ -3,6 +3,8 @@ package com.android.streetworkapp.ui.park
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
@@ -13,22 +15,28 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.android.streetworkapp.model.event.Event
+import com.android.streetworkapp.utils.toFormattedString
+
+@Composable
+fun EventItemList(events: List<Event>) {
+  LazyColumn { items(events) { event -> EventItem(event = event) } }
+}
 
 @Composable
 fun EventItem(event: Event) {
   ListItem(
-      headlineContent = { Text(text = event.title, lineHeight = 18.sp) },
+      headlineContent = { Text(text = event.title) },
       supportingContent = {
         Text(
             "Participants ${event.participants}/${event.maxParticipants}",
             fontWeight = FontWeight.Light)
       },
-      overlineContent = { Text("31/10/2024 17:00", fontWeight = FontWeight.Bold) },
+      overlineContent = {
+        Text(text = event.date.toFormattedString(), fontWeight = FontWeight.Bold)
+      },
       leadingContent = {
         Icon(
             imageVector = Icons.Default.AccountCircle,
@@ -40,7 +48,6 @@ fun EventItem(event: Event) {
             onClick = { TODO("Go to the event overview") },
             modifier = Modifier.size(width = 80.dp, height = 48.dp),
             colors = ButtonDefaults.buttonColors(),
-            shape = RectangleShape,
             contentPadding = PaddingValues(0.dp)) {
               Text("About")
             }
