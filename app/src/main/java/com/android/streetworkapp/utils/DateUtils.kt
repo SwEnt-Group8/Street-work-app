fa36b3cf030b343a82c3197987e9ceffe5bab4b5
Arthur Muster <arthur@must-r.com>
2024-10-07 16:32:45 +0200

    chore: format code using ktfmt

--- app/src/main/java/com/android/streetworkapp/utils/DateUtils.kt
+++ app/src/main/java/com/android/streetworkapp/utils/DateUtils.kt
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 fun Timestamp.toFormattedString(): String {
-    val pattern = "dd/MM/yyyy HH:mm"
-    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
-    return sdf.format(this.toDate())
+  val pattern = "dd/MM/yyyy HH:mm"
+  val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+  return sdf.format(this.toDate())
 }
