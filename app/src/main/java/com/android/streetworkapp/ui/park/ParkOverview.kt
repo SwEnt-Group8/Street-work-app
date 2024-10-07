ebd4612ccce990fbae99e875ce07a6744f334f6d
Arthur Muster <arthurmuster@tsf-452-wpa-0-141.epfl.ch>
2024-10-07 14:42:59 +0200

    feat(ui): add ParkOverview file and EventItem composable

---
+++ app/src/main/java/com/android/streetworkapp/ui/park/ParkOverview.kt
@@ -0,0 +1,43 @@
+package com.android.streetworkapp.ui.park
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EventItem() {
+  ListItem(
+      headlineContent = { Text("Alice started a freestyle contest ", lineHeight = 18.sp) },
+      supportingContent = { Text("Participants 5/7") },
+      leadingContent = {
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "Profile Icon",
+            modifier = Modifier.size(56.dp))
+      },
+      trailingContent = {
+        Button(
+            onClick = {},
+            modifier = Modifier.size(width = 80.dp, height = 48.dp),
+            colors = ButtonDefaults.buttonColors(),
+            shape = RectangleShape,
+            contentPadding = PaddingValues(0.dp)) {
+              Text("About")
+            }
+      },
+      modifier = Modifier.padding(8.dp))
+  HorizontalDivider()
+}
