4f1b46b7a8338c78a2fbc4e5638e9f2974e30d73
Arthur Muster <arthur@must-r.com>
2024-10-07 16:31:46 +0200

    feat(utils): add DateUtils file with date formatting function

---
+++ app/src/main/java/com/android/streetworkapp/utils/DateUtils.kt
@@ -0,0 +1,11 @@
+package com.android.streetworkapp.utils
+
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+fun Timestamp.toFormattedString(): String {
+    val pattern = "dd/MM/yyyy HH:mm"
+    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+    return sdf.format(this.toDate())
+}
