3b93450ee3d7635460f19ac756c868f5d5473ab8
tercierp <paul.tercier@epfl.ch>
2024-10-09 10:38:32 +0200

    test(map): Changed the way Map.kt is tested
    Moved roboelectric deletion test due to unstable behavior when run on IC. Added tests with instrumentalization instead of roboelectric.

--- app/src/androidTest/java/com/android/streetworkapp/ui/map/Map.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/map/Map.kt
@@ -4,9 +4,15 @@ import android.Manifest
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.core.app.ActivityOptionsCompat
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
 import com.android.streetworkapp.MainActivity
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
@@ -14,12 +20,16 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.shadows.ShadowApplication
 
 @RunWith(AndroidJUnit4::class)
 class MapTest {
 
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  // GrantPermissionRule is used to automatically grant permissions
+  @get:Rule
+  val grantPermissionRule: GrantPermissionRule =
+      GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION)
 
   private lateinit var requestPermissionLauncher: FakeActivityResultLauncher
 
@@ -29,28 +39,32 @@ class MapTest {
   }
 
   @Test
-  fun testRequestPermissionWhenNotGranted() {
-
+  fun buttonClick() {
     composeTestRule.activityRule.scenario.onActivity { activity ->
-      // Simulate that the permission is denied for this test case
-      ShadowApplication.getInstance().denyPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
-      activity.setContent { Map(requestPermissionLauncher = requestPermissionLauncher) }
+      activity.setContent { MapPermission(requestPermissionLauncher = requestPermissionLauncher) }
     }
-    // Assert that the permission request was triggered
-    assertNotNull(requestPermissionLauncher.launchedPermission) {
-      "Expected permission not null but found: ${requestPermissionLauncher.launchedPermission}"
-    }
+    composeTestRule.onNodeWithTag("requestPermissionButton").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("requestPermissionButton").performClick()
+    composeTestRule.onNodeWithText("Test Permission").assertIsDisplayed()
+
+    // Assert that clicking the button triggers a permission request
+    assertNotNull(
+      "Expected permission not null but found: ${requestPermissionLauncher.launchedPermission}",
+      requestPermissionLauncher.launchedPermission
+    )
   }
 
   @Test
   fun testDoNotRequestPermissionWhenGranted() {
     composeTestRule.activityRule.scenario.onActivity { activity ->
-      // Simulate that the permission is already granted
-      ShadowApplication.getInstance().grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+      // Grant permission using the InstrumentationRegistry for testing purposes
+      val instrumentation = InstrumentationRegistry.getInstrumentation()
+      val context = instrumentation.targetContext
+      instrumentation.uiAutomation.grantRuntimePermission(
+          context.packageName, Manifest.permission.ACCESS_FINE_LOCATION)
 
-      activity.setContent { Map(requestPermissionLauncher = requestPermissionLauncher) }
+      activity.setContent { MapPermission(requestPermissionLauncher = requestPermissionLauncher) }
     }
-
     // Assert that the permission request was not triggered
     assertNull(requestPermissionLauncher.launchedPermission)
   }
--- app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
+++ app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
@@ -4,21 +4,25 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.core.content.ContextCompat
 
 @Composable
-fun Map(requestPermissionLauncher: ActivityResultLauncher<String>) {
+fun MapPermission(requestPermissionLauncher: ActivityResultLauncher<String>) {
   val context = LocalContext.current
   val hasLocationPermission by remember {
     mutableStateOf(
-        ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED)
+      ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+              PackageManager.PERMISSION_GRANTED)
   }
 
   // Request location permission if not granted
@@ -27,5 +31,9 @@ fun Map(requestPermissionLauncher: ActivityResultLauncher<String>) {
       Log.d("Map", "Requesting permission: permission.ACCESS_FINE_LOCATION")
       requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
+  }
+  //Could be remove depends on the map implementation
+  Button(onClick = { requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION) }, modifier = Modifier.testTag("requestPermissionButton")) {
+    Text(text = "Test Permission")
   }
 }
