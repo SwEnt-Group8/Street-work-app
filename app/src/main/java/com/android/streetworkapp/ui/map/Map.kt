6f3e61ca99df5c07f74308823c31bd7f04dba42c
tercierp <paul.tercier@epfl.ch>
2024-10-09 23:42:56 +0200

    feat(map): add a palette and put more style

--- app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
+++ app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
@@ -4,17 +4,24 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import com.android.streetworkapp.ui.theme.WoodCharcoal
 
 @Composable
 fun MapManager(requestPermissionLauncher: ActivityResultLauncher<String>) {
@@ -33,9 +40,16 @@ fun MapManager(requestPermissionLauncher: ActivityResultLauncher<String>) {
     }
   }
   // Could be remove depends on the map implementation
-  Button(
-      onClick = { requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION) },
-      modifier = Modifier.testTag("requestPermissionButton")) {
-        Text(text = "Test Permission")
+  Box(
+      modifier = Modifier.fillMaxSize().padding(16.dp).testTag("BoxMap"),
+      contentAlignment = Alignment.Center) {
+        Button(
+            onClick = {
+              requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            },
+            modifier = Modifier.testTag("requestPermissionButton"),
+            colors = ButtonDefaults.buttonColors(WoodCharcoal)) {
+              Text(text = "Test Permission")
+            }
       }
 }
