6a5061848e63a9b72a5b5e06d8d2ce48ab4c2083
tercierp <paul.tercier@epfl.ch>
2024-10-09 21:57:31 +0200

    chore(format): apply ktfmt formatting

--- app/src/main/java/com/android/streetworkapp/SimpleData.kt
+++ app/src/main/java/com/android/streetworkapp/SimpleData.kt
@@ -4,9 +4,9 @@ import kotlin.math.sqrt
 
 data class Point(val x: Double, val y: Double) {
 
-    fun distanceTo(p: Point): Double {
-        val dx = x - p.x
-        val dy = y - p.y
-        return sqrt(dx * dx + dy * dy)
-    }
+  fun distanceTo(p: Point): Double {
+    val dx = x - p.x
+    val dy = y - p.y
+    return sqrt(dx * dx + dy * dy)
+  }
 }
