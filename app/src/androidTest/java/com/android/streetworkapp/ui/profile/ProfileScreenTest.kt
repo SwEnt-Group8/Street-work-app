6f93728de303cadfbdd3f7920bb6c687fb9ed605
Kemin <kemin.zheng@epfl.ch>
2024-10-10 19:32:43 +0200

    fix(test): reformat to ktfmt

--- app/src/androidTest/java/com/android/streetworkapp/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/profile/ProfileScreenTest.kt
@@ -1,7 +1,6 @@
 package com.android.streetworkapp.ui.profile
 
 import androidx.compose.ui.test.assertHasClickAction
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
