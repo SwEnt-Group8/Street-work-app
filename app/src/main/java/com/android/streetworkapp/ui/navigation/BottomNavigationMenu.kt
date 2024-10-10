9bc3afe04b768f3a3f931fa8e9bd48d53d0752fc
Malick Sy <malicksy2002@gmail.com>
2024-10-10 18:01:52 +0200

    chore: reverted mainActivity to its original state in main for consistency

--- app/src/main/java/com/android/streetworkapp/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/streetworkapp/ui/navigation/BottomNavigationMenu.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
