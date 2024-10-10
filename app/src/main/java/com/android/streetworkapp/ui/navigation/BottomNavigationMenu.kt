186a4f7f9d1c16381558491ea48eff218e8527b5
Kemin <kemin.zheng@epfl.ch>
2024-10-10 10:54:13 +0200

    feat(nav): add BottomNavigationMenu

---
+++ app/src/main/java/com/android/streetworkapp/ui/navigation/BottomNavigationMenu.kt
@@ -0,0 +1,41 @@
+package com.github.se.bootcamp.ui.navigation
+
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import com.android.streetworkapp.ui.navigation.TopLevelDestination
+
+@Composable
+fun BottomNavigationMenu(
+    onTabSelect: (TopLevelDestination) -> Unit,
+    tabList: List<TopLevelDestination>,
+    selectedItem: String
+) {
+  NavigationBar(
+      modifier = Modifier.fillMaxWidth().height(90.dp).testTag("bottomNavigationMenu"),
+      containerColor = MaterialTheme.colorScheme.surfaceDim,
+      content = {
+        tabList.forEach { tab ->
+          NavigationBarItem(
+              icon = { Icon(tab.icon, contentDescription = null) },
+              label = { Text(tab.textId) },
+              selected = tab.route == selectedItem,
+              onClick = { onTabSelect(tab) },
+              modifier = Modifier.clip(RoundedCornerShape(50.dp)).testTag(tab.textId))
+        }
+      },
+  )
+}
