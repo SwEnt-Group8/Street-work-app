e1de166660e4a8e595f154e3399122dcda3bd311
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-10-14 12:40:47 +0200

    chore: create file AddEvent.kt with function signatures

---
+++ app/src/main/java/com/android/streetworkapp/ui/park/AddEvent.kt
@@ -0,0 +1,12 @@
+package com.android.streetworkapp.ui.park
+
+import androidx.compose.runtime.Composable
+
+/** Display a view that is used to add a new Event to a given park. */
+@Composable fun AddEventScreen() {}
+
+@Composable fun EventTypeSelection() {}
+
+@Composable fun ParticipantNumberSelection() {}
+
+@Composable fun TimeSelection() {}
