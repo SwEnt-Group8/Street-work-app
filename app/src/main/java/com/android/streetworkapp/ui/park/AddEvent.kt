34702ff8acbabfc239455a26c0fc3c89380e8c70
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-10-15 15:15:32 +0200

    feat: Implement a slider for selecting the maximum number of participants

--- app/src/main/java/com/android/streetworkapp/ui/park/AddEvent.kt
+++ app/src/main/java/com/android/streetworkapp/ui/park/AddEvent.kt
@@ -1,12 +1,53 @@
 package com.android.streetworkapp.ui.park
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.android.streetworkapp.model.event.Event
 
 /** Display a view that is used to add a new Event to a given park. */
 @Composable fun AddEventScreen() {}
 
 @Composable fun EventTypeSelection() {}
 
-@Composable fun ParticipantNumberSelection() {}
+@Composable
+fun ParticipantNumberSelection(event: Event) {
+  val minParticipants = 2 // We need at least 2 users for every event
+  val maxParticipants = 10 // An event can not have more then 10 participants
+  var sliderPosition by remember { mutableFloatStateOf(minParticipants.toFloat()) }
+
+  Column(
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier.fillMaxWidth()) {
+        Slider(
+            modifier = Modifier.fillMaxWidth(0.8f),
+            value = sliderPosition,
+            onValueChange = {
+              sliderPosition = it
+              event.maxParticipants = sliderPosition.toInt()
+            },
+            colors =
+                SliderDefaults.colors(
+                    thumbColor = MaterialTheme.colorScheme.secondary,
+                    activeTrackColor = MaterialTheme.colorScheme.secondary,
+                    inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                ),
+            steps = 7,
+            valueRange = minParticipants.toFloat()..maxParticipants.toFloat())
+        Text(text = sliderPosition.toInt().toString())
+      }
+}
 
 @Composable fun TimeSelection() {}
