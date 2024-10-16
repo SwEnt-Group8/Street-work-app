96a1bb5683b712c78ab332dccb6dc0a55b18db5e
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-10-16 16:33:11 +0200

    feat: implement a date and hour selection composable

--- app/src/main/java/com/android/streetworkapp/ui/park/AddEvent.kt
+++ app/src/main/java/com/android/streetworkapp/ui/park/AddEvent.kt
@@ -1,21 +1,51 @@
 package com.android.streetworkapp.ui.park
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import com.android.streetworkapp.model.event.Event
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
 
 /** Display a view that is used to add a new Event to a given park. */
 @Composable fun AddEventScreen() {}
@@ -51,4 +81,105 @@ fun ParticipantNumberSelection(event: Event) {
       }
 }
 
-@Composable fun TimeSelection() {}
+@ExperimentalMaterial3Api
+@Composable
+fun TimeSelection(event: Event) {
+  var showDatePicker by remember { mutableStateOf(false) }
+  var showTimePicker by remember { mutableStateOf(false) }
+  val datePickerState = rememberDatePickerState()
+
+  val currentTime = Calendar.getInstance()
+  val timePickerState =
+      rememberTimePickerState(
+          initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+          initialMinute = currentTime.get(Calendar.MINUTE),
+          is24Hour = false,
+      )
+
+  val currentTimeSelection =
+      datePickerState.selectedDateMillis?.plus(
+          (TimeUnit.HOURS.toMillis(timePickerState.hour.toLong()) +
+              TimeUnit.MINUTES.toMillis(timePickerState.minute.toLong())))
+
+  val selectedDate =
+      datePickerState.selectedDateMillis?.let { convertMillisToDate(it + currentTimeSelection!!) }
+          ?: ""
+
+  Box(modifier = Modifier.fillMaxWidth()) {
+    OutlinedTextField(
+        value = selectedDate,
+        onValueChange = {},
+        label = { Text("When do you want to train?") },
+        readOnly = true,
+        trailingIcon = {
+          Row {
+            IconButton(onClick = { showDatePicker = !showDatePicker }) {
+              Icon(imageVector = Icons.Default.DateRange, contentDescription = "Select date")
+            }
+            IconButton(onClick = { showTimePicker = !showTimePicker }) {
+              Icon(imageVector = Icons.Default.AddCircle, contentDescription = "Select date")
+            }
+          }
+        },
+        modifier = Modifier.fillMaxWidth().height(64.dp))
+
+    if (showDatePicker) {
+      Popup(onDismissRequest = { showDatePicker = false }, alignment = Alignment.TopStart) {
+        Box(
+            modifier =
+                Modifier.fillMaxWidth()
+                    .offset(y = 64.dp)
+                    .shadow(elevation = 4.dp)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(16.dp)) {
+              DatePicker(state = datePickerState, showModeToggle = false)
+            }
+
+        Button(
+            modifier = Modifier.offset(x = 280.dp, y = 140.dp),
+            onClick = {
+              showDatePicker = false
+
+              event.date =
+                  datePickerState.selectedDateMillis
+                      ?.let { TimeUnit.MILLISECONDS.toSeconds(it + currentTimeSelection!!) }
+                      ?.let { Timestamp(it, 0) }!!
+            }) {
+              Text("Validate")
+            }
+      }
+    }
+
+    if (showTimePicker) {
+      Popup(onDismissRequest = { showTimePicker = false }, alignment = Alignment.TopStart) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier =
+                Modifier.fillMaxSize()
+                    .offset(y = 68.dp)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .align(Alignment.Center)) {
+              TimePicker(state = timePickerState, modifier = Modifier.fillMaxWidth())
+
+              Button(
+                  onClick = {
+                    showTimePicker = false
+
+                    event.date =
+                        datePickerState.selectedDateMillis
+                            ?.let { TimeUnit.MILLISECONDS.toSeconds(it + currentTimeSelection!!) }
+                            ?.let { Timestamp(it, 0) }!!
+                  }) {
+                    Text("Validate")
+                  }
+            }
+      }
+    }
+  }
+}
+
+private fun convertMillisToDate(millis: Long): String {
+  val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+  formatter.timeZone = TimeZone.getTimeZone("UTC")
+  return formatter.format(Date(millis))
+}
