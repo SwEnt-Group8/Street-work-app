6b4a491586f5a3944aa3b658d35793fff1be9b66
Arthur Muster <arthur@must-r.com>
2024-10-17 20:47:45 +0200

    fix(park): use the view model scope launch for ParkViewModel

--- app/src/main/java/com/android/streetworkapp/model/park/ParkViewModel.kt
+++ app/src/main/java/com/android/streetworkapp/model/park/ParkViewModel.kt
@@ -1,6 +1,8 @@
 package com.android.streetworkapp.model.park
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
 
@@ -19,9 +21,7 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @return The park with the given ID, or null if the park does not exist.
    */
-  suspend fun getParkByPid(pid: String): Park? {
-    return repository.getParkByPid(pid)
-  }
+  fun getParkByPid(pid: String) = viewModelScope.launch { repository.getParkByPid(pid) }
 
   /**
    * Get a park by its location ID.
@@ -29,18 +29,15 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param locationId The location ID.
    * @return The park with the given location ID, or null if the park does not exist.
    */
-  suspend fun getParkByLocationId(locationId: String): Park? {
-    return repository.getParkByLocationId(locationId)
-  }
+  fun getParkByLocationId(locationId: String) =
+      viewModelScope.launch { repository.getParkByLocationId(locationId) }
 
   /**
    * Create a park.
    *
    * @param park The park to create.
    */
-  suspend fun createPark(park: Park) {
-    repository.createPark(park)
-  }
+  fun createPark(park: Park) = viewModelScope.launch { repository.createPark(park) }
 
   /**
    * Update the name of a park.
@@ -48,9 +45,8 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param name The new name.
    */
-  suspend fun updateName(pid: String, name: String) {
-    repository.updateName(pid, name)
-  }
+  fun updateName(pid: String, name: String) =
+      viewModelScope.launch { repository.updateName(pid, name) }
 
   /**
    * Update the image reference of a park.
@@ -58,9 +54,8 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param imageReference The new image reference.
    */
-  suspend fun updateImageReference(pid: String, imageReference: String) {
-    repository.updateImageReference(pid, imageReference)
-  }
+  fun updateImageReference(pid: String, imageReference: String) =
+      viewModelScope.launch { repository.updateImageReference(pid, imageReference) }
 
   /**
    * Add a rating to a park.
@@ -68,9 +63,8 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param rating The rating to add.
    */
-  suspend fun addRating(pid: String, rating: Int) {
-    repository.addRating(pid, rating)
-  }
+  fun addRating(pid: String, rating: Int) =
+      viewModelScope.launch { repository.addRating(pid, rating) }
 
   /**
    * Delete a rating from a park.
@@ -78,9 +72,8 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param rating The rating to delete.
    */
-  suspend fun deleteRating(pid: String, rating: Int) {
-    repository.deleteRating(pid, rating)
-  }
+  fun deleteRating(pid: String, rating: Int) =
+      viewModelScope.launch { repository.deleteRating(pid, rating) }
 
   /**
    * Update the capacity of a park.
@@ -88,27 +81,22 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param capacity The new capacity.
    */
-  suspend fun updateCapacity(pid: String, capacity: Int) {
-    repository.updateCapacity(pid, capacity)
-  }
+  fun updateCapacity(pid: String, capacity: Int) =
+      viewModelScope.launch { repository.updateCapacity(pid, capacity) }
 
   /**
    * Increment the occupancy of a park.
    *
    * @param pid The park ID.
    */
-  suspend fun incrementOccupancy(pid: String) {
-    repository.incrementOccupancy(pid)
-  }
+  fun incrementOccupancy(pid: String) = viewModelScope.launch { repository.incrementOccupancy(pid) }
 
   /**
    * Decrement the occupancy of a park.
    *
    * @param pid The park ID.
    */
-  suspend fun decrementOccupancy(pid: String) {
-    repository.decrementOccupancy(pid)
-  }
+  fun decrementOccupancy(pid: String) = viewModelScope.launch { repository.decrementOccupancy(pid) }
 
   /**
    * Add an event to a park.
@@ -116,9 +104,8 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param eid The event ID.
    */
-  suspend fun addEventToPark(pid: String, eid: String) {
-    repository.addEventToPark(pid, eid)
-  }
+  fun addEventToPark(pid: String, eid: String) =
+      viewModelScope.launch { repository.addEventToPark(pid, eid) }
 
   /**
    * Delete an event from a park.
@@ -126,16 +113,13 @@ open class ParkViewModel(private val repository: ParkRepository) : ViewModel() {
    * @param pid The park ID.
    * @param eid The event ID.
    */
-  suspend fun deleteEventFromPark(pid: String, eid: String) {
-    repository.deleteEventFromPark(pid, eid)
-  }
+  fun deleteEventFromPark(pid: String, eid: String) =
+      viewModelScope.launch { repository.deleteEventFromPark(pid, eid) }
 
   /**
    * Delete a park by its ID.
    *
    * @param pid The park ID.
    */
-  suspend fun deleteParkByPid(pid: String) {
-    repository.deleteParkByPid(pid)
-  }
+  fun deleteParkByPid(pid: String) = viewModelScope.launch { repository.deleteParkByPid(pid) }
 }
