1e5601de61b74eb1417ceb827f8e6d22bdc8095c
tercierp <paul.tercier@epfl.ch>
2024-11-08 23:42:30 +0100

    feat(park): made `Park` deserialize

--- app/src/main/java/com/android/streetworkapp/model/park/Park.kt
+++ app/src/main/java/com/android/streetworkapp/model/park/Park.kt
@@ -16,14 +16,14 @@ import com.android.streetworkapp.model.parklocation.ParkLocation
  * @param votersUIDs The list of UIDs of users who have voted.
  */
 data class Park(
-    val pid: String,
-    var name: String,
-    var location: ParkLocation,
-    var imageReference: String,
+    val pid: String = "",
+    var name: String = "",
+    var location: ParkLocation = ParkLocation(0.0, 0.0, ""),
+    var imageReference: String = "",
     var rating: Float = 0f,
     var nbrRating: Int = 0,
-    var capacity: Int,
-    var occupancy: Int,
-    var events: List<String>,
+    var capacity: Int = 0,
+    var occupancy: Int = 0,
+    var events: List<String> = emptyList(),
     var votersUIDs: List<String> = emptyList()
 )
