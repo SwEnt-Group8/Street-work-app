77d21f9b785bb14c869a94fa99cf60577fa8f14e
tercierp <paul.tercier@epfl.ch>
2024-11-08 18:43:24 +0100

    feat(park): remove some useless var

--- app/src/main/java/com/android/streetworkapp/model/park/Park.kt
+++ app/src/main/java/com/android/streetworkapp/model/park/Park.kt
@@ -13,8 +13,6 @@ import com.android.streetworkapp.model.parklocation.ParkLocation
  * @param nbrRating The number of ratings.
  * @param occupancy The park occupancy.
  * @param events The list of events in the park.
- * @param voterWeight The number of voters (weight).
- * @param averageRating The current average rating.
  * @param votersUIDs The list of UIDs of users who have voted.
  */
 data class Park(
@@ -22,12 +20,10 @@ data class Park(
     var name: String,
     var location: ParkLocation,
     var imageReference: String,
-    var rating: Float,
-    var nbrRating: Int,
+    var rating: Float = 0f,
+    var nbrRating: Int = 0,
     var capacity: Int,
     var occupancy: Int,
     var events: List<String>,
-    var voterWeight: Int = 0,
-    var averageRating: Float = 0f,
     var votersUIDs: List<String> = emptyList()
 )
