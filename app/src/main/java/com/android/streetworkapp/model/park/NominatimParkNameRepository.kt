a89530755c813cf67f6e8e5b735b6a0014e8bbb2
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-11-11 21:30:14 +0100

    feat: implement convertLocationIdToParkName function

--- app/src/main/java/com/android/streetworkapp/model/park/NominatimParkNameRepository.kt
+++ app/src/main/java/com/android/streetworkapp/model/park/NominatimParkNameRepository.kt
@@ -1,6 +1,13 @@
 package com.android.streetworkapp.model.park
 
+import java.io.IOException
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 
 class NominatimParkNameRepository(val client: OkHttpClient) : ParkNameRepository {
 
@@ -15,6 +22,39 @@ class NominatimParkNameRepository(val client: OkHttpClient) : ParkNameRepository
       onSuccess: (String) -> Unit,
       onFailure: (Exception) -> Unit
   ) {
-    TODO("Not yet implemented")
+
+    // Based on the Nominatim API. More information: https://nominatim.org/
+
+    val url =
+        HttpUrl.Builder()
+            .scheme("https")
+            .host("nominatim.openstreetmap.org")
+            .addPathSegment("lookup")
+            .addQueryParameter("osm_ids", "W$locationId")
+            .addQueryParameter("format", "json")
+            .build()
+
+    val request = Request.Builder().url(url).header("User-Agent", "test/5.0").build()
+
+    client
+        .newCall(request)
+        .enqueue(
+            object : Callback {
+              override fun onFailure(call: Call, e: IOException) {
+                onFailure(e)
+              }
+
+              override fun onResponse(call: Call, response: Response) {
+                response.body?.let { decodeRoadJson(it.string()) }?.let { onSuccess(it) }
+              }
+            })
+  }
+
+  fun decodeRoadJson(json: String): String {
+    require(json.isNotEmpty())
+    val jsonObject = JSONObject(json)
+    val array = jsonObject.getJSONArray("address")
+    val roadName = array.getString(0)
+    return roadName
   }
 }
