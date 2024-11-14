9e77e1b8a1a5b00cd71a21ea47767e43c5625ed8
tercierp <paul.tercier@epfl.ch>
2024-11-14 18:27:07 +0100

    feat(ui): update the `BluetoothButton` like Figma

--- app/src/main/java/com/android/streetworkapp/ui/profile/BluetoothButton.kt
+++ app/src/main/java/com/android/streetworkapp/ui/profile/BluetoothButton.kt
@@ -1,8 +1,11 @@
 package com.android.streetworkapp.ui.profile
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,20 +14,32 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.streetworkapp.device.bluetooth.BluetoothServer
+import com.android.streetworkapp.ui.theme.ColorPalette.INTERACTION_COLOR_DARK
+import com.android.streetworkapp.ui.theme.ColorPalette.PRIMARY_TEXT_COLOR
 
 @Composable
 fun BluetoothButton(
     bluetoothController: BluetoothServer,
     uid: String // UID to send
 ) {
-  val context = LocalContext.current
+    val context = LocalContext.current
 
-  Button(
-      onClick = {
-        bluetoothController.startGattServer(uid) // Start GATT server to send UID
-        Toast.makeText(context, "Broadcasting UID via BLE GATT server", Toast.LENGTH_SHORT).show()
-      },
-      modifier = Modifier.size(220.dp, 50.dp).testTag("BluetoothButton")) {
-        Text(text = "Broadcast UID", fontSize = 17.sp)
-      }
+    Button(
+        onClick = {
+            bluetoothController.startGattServer(uid) // Start GATT server to send UID
+            Toast.makeText(context, "Send friend request", Toast.LENGTH_SHORT).show()
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = INTERACTION_COLOR_DARK),
+        modifier = Modifier
+            .width(220.dp)
+            .height(50.dp)
+            .testTag("BluetoothButton")
+    ) {
+        Text(
+            text = "Send request",
+            fontSize = 17.sp,
+            color = PRIMARY_TEXT_COLOR
+        )
+    }
 }
+
