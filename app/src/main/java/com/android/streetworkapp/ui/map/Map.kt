ffa29801eccce801a6f35c901ace937970484e21
Kemin <kemin.zhengepfl.ch>
2024-11-14 14:39:15 +0100

    fix(MapUiTest): abstracted permission and location function. Needed for Unit test

--- app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
+++ app/src/main/java/com/android/streetworkapp/ui/map/Map.kt
@@ -1,8 +1,5 @@
 package com.android.streetworkapp.ui.map
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -15,26 +12,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
+import com.android.streetworkapp.utils.LocationService
+import com.android.streetworkapp.utils.PermissionManager
 import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.LatLng
 
 @Composable
 fun MapManager(
     userLocation: MutableState<LatLng>,
-    onUserLocationChange: (MutableState<LatLng>) -> Unit
+    onUserLocationChange: (MutableState<LatLng>) -> Unit,
+    permissionManager: PermissionManager,
+    locationService: LocationService
 ) {
   val context = LocalContext.current
 
-  // State whether the user has granted location permission
   val hasLocationPermission by remember {
-    mutableStateOf(
-        ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED)
+    mutableStateOf(permissionManager.hasLocationPermission())
   }
 
   // Request permission launcher
@@ -56,21 +50,10 @@ fun MapManager(
   // Request location permission if not granted
   LaunchedEffect(hasLocationPermission) {
     if (!hasLocationPermission) {
-      Log.d("Map", "requestPermissionLauncher.launch")
-      requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+      permissionManager.requestLocationPermission(requestPermissionLauncher)
     }
   }
 
-  // Define the FusedLocationProviderClient to get location
-  val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-
-  // Define the location request process
-  val locationRequest =
-      LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000) // update every 10s
-          .setMinUpdateIntervalMillis(5000) // Minimum update interval (can't be faster)
-          .build()
-
-  // update userLocation each uppdate
   val locationCallback = remember {
     object : LocationCallback() {
       override fun onLocationResult(locationResult: LocationResult) {
@@ -85,17 +68,9 @@ fun MapManager(
   // Start location updates when it have permission
   LaunchedEffect(hasLocationPermission) {
     if (hasLocationPermission) {
-      try {
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest, locationCallback, Looper.getMainLooper())
-      } catch (e: SecurityException) {
-        Log.e("Map", "Location permission revoked.")
-      }
+      locationService.startLocationUpdates(locationCallback)
     }
   }
-
   // Stop location updates
-  DisposableEffect(Unit) {
-    onDispose { fusedLocationClient.removeLocationUpdates(locationCallback) }
-  }
+  DisposableEffect(Unit) { onDispose { locationService.stopLocationUpdates(locationCallback) } }
 }
