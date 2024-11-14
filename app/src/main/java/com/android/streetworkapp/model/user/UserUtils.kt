d3e5bd11bf8d646d3aa49741f46c8ac6e6d84666
Arthur Muster <arthur@must-r.com>
2024-11-14 10:15:51 +0100

    fix(user): make UserUtils not a class

--- app/src/main/java/com/android/streetworkapp/model/user/UserUtils.kt
+++ app/src/main/java/com/android/streetworkapp/model/user/UserUtils.kt
@@ -2,22 +2,18 @@ package com.android.streetworkapp.model.user
 
 import com.google.firebase.auth.FirebaseUser
 
-/** A utility class for the user model. */
-class UserUtils {
-
-  /**
-   * Creates a new [User] object from the provided [FirebaseUser].
-   *
-   * @param firebaseUser The FirebaseUser object to create the User object from.
-   * @return The User object created from the FirebaseUser.
-   */
-  fun createNewUserFromFirebaseUser(firebaseUser: FirebaseUser): User {
-    require(firebaseUser.uid.isNotEmpty()) { "UID must not be empty" }
-    return User(
-        uid = firebaseUser.uid,
-        username = firebaseUser.displayName ?: "",
-        email = firebaseUser.email ?: "",
-        score = 0,
-        friends = emptyList())
-  }
+/**
+ * Creates a new [User] object from the provided [FirebaseUser].
+ *
+ * @param firebaseUser The FirebaseUser object to create the User object from.
+ * @return The User object created from the FirebaseUser.
+ */
+fun createNewUserFromFirebaseUser(firebaseUser: FirebaseUser): User {
+  require(firebaseUser.uid.isNotEmpty()) { "UID must not be empty" }
+  return User(
+      uid = firebaseUser.uid,
+      username = firebaseUser.displayName ?: "",
+      email = firebaseUser.email ?: "",
+      score = 0,
+      friends = emptyList())
 }
