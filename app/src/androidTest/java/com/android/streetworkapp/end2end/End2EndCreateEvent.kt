68dd18c1c49e8310b6290fc32a579b7358b38349
Malick Sy <malicksy2002@gmail.com>
2024-11-15 09:36:29 +0100

    fix: fixed conflicting errors between some commits

--- app/src/androidTest/java/com/android/streetworkapp/end2end/End2EndCreateEvent.kt
+++ app/src/androidTest/java/com/android/streetworkapp/end2end/End2EndCreateEvent.kt
@@ -45,186 +45,193 @@ import org.mockito.Mockito.mock
 @RunWith(AndroidJUnit4::class)
 class End2EndCreateEvent {
 
-    @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    // repositories and viewmodels tested in this end2end test
-    private lateinit var firestoreDB: FirebaseFirestore
-    private lateinit var parkLocationRepository: OverpassParkLocationRepository
-    private lateinit var parkNameRepository: NominatimParkNameRepository
-    private lateinit var parkRepository: ParkRepository
-    private lateinit var eventRepository: EventRepository
-    private lateinit var parkLocationViewModel: ParkLocationViewModel
-    private lateinit var userViewModel: UserViewModel
-    private lateinit var parkViewModel: ParkViewModel
-    private lateinit var eventViewModel: EventViewModel
+  // repositories and viewmodels tested in this end2end test
+  private lateinit var firestoreDB: FirebaseFirestore
+  private lateinit var parkLocationRepository: OverpassParkLocationRepository
+  private lateinit var parkNameRepository: NominatimParkNameRepository
+  private lateinit var parkRepository: ParkRepository
+  private lateinit var eventRepository: EventRepository
+  private lateinit var parkLocationViewModel: ParkLocationViewModel
+  private lateinit var userViewModel: UserViewModel
+  private lateinit var parkViewModel: ParkViewModel
+  private lateinit var eventViewModel: EventViewModel
 
-    // mock event
-    private lateinit var testEvent: Event
+  // mock event
+  private lateinit var testEvent: Event
 
-    // mocked repository not used in this test
-    private lateinit var userRepository: UserRepository
+  // mocked repository not used in this test
+  private lateinit var userRepository: UserRepository
 
-    // Boolean to check if the map is loaded
-    private var mapISLoaded = false
+  // Boolean to check if the map is loaded
+  private var mapISLoaded = false
 
-    // Define a state variable to hold the current screen
-    private var currentScreen by mutableStateOf(Screen.MAP)
+  // Define a state variable to hold the current screen
+  private var currentScreen by mutableStateOf(Screen.MAP)
 
-    @Before
-    fun setUp() {
+  @Before
+  fun setUp() {
 
-        // Instantiate fire store database and associated user repository :
-        firestoreDB = FirebaseFirestore.getInstance()
+    // Instantiate fire store database and associated user repository :
+    firestoreDB = FirebaseFirestore.getInstance()
 
-        // delete all data in the testParks collection for consistency
-        val eventsCollection = firestoreDB.collection("testParks")
+    // delete all data in the testParks collection for consistency
+    val eventsCollection = firestoreDB.collection("testParks")
 
-        eventsCollection
-            .get()
-            .addOnSuccessListener { querySnapshot ->
-                for (document in querySnapshot.documents) {
-                    document.reference
-                        .delete()
-                        .addOnSuccessListener { println("Event ${document.id} deleted successfully") }
-                        .addOnFailureListener { e ->
-                            println("Error deleting event ${document.id}: ${e.message}")
-                        }
+    eventsCollection
+        .get()
+        .addOnSuccessListener { querySnapshot ->
+          for (document in querySnapshot.documents) {
+            document.reference
+                .delete()
+                .addOnSuccessListener { println("Event ${document.id} deleted successfully") }
+                .addOnFailureListener { e ->
+                  println("Error deleting event ${document.id}: ${e.message}")
                 }
-            }
-            .addOnFailureListener { e -> println("Error retrieving events: ${e.message}") }
-
-        // repositories
-        parkLocationRepository = OverpassParkLocationRepository(OkHttpClient())
-        parkNameRepository = NominatimParkNameRepository(OkHttpClient())
-        eventRepository = EventRepositoryFirestore(firestoreDB)
-        userRepository = mock(UserRepository::class.java)
-        parkRepository = ParkRepositoryFirestore(firestoreDB, testing = true)
-
-        // viewmodels
-        parkLocationViewModel = ParkLocationViewModel(parkLocationRepository)
-        userViewModel = UserViewModel(userRepository)
-        parkViewModel = ParkViewModel(parkRepository, parkNameRepository)
-        eventViewModel = EventViewModel(eventRepository)
-
-        testEvent =
-            Event(
-                eid = "13413555968",
-                title = "E2ECreateEvent",
-                description = "This event aims to test the creation of an event",
-                participants = 1,
-                maxParticipants = 2,
-                date = Timestamp(0, 0), // 01/01/1970 00:00
-                owner = "E2ECreateEventOwner")
-
-        composeTestRule.setContent {
-            StreetWorkApp(
-                parkLocationViewModel,
-                { navigateTo(currentScreen) },
-                { mapISLoaded = true },
-                userViewModel,
-                parkViewModel,
-                eventViewModel)
+          }
         }
+        .addOnFailureListener { e -> println("Error retrieving events: ${e.message}") }
+
+    // repositories
+    parkLocationRepository = OverpassParkLocationRepository(OkHttpClient())
+    parkNameRepository = NominatimParkNameRepository(OkHttpClient())
+    eventRepository = EventRepositoryFirestore(firestoreDB)
+    userRepository = mock(UserRepository::class.java)
+    parkRepository = ParkRepositoryFirestore(firestoreDB, testing = true)
+
+    // viewmodels
+    parkLocationViewModel = ParkLocationViewModel(parkLocationRepository)
+    userViewModel = UserViewModel(userRepository)
+    parkViewModel = ParkViewModel(parkRepository, parkNameRepository)
+    eventViewModel = EventViewModel(eventRepository)
+
+    testEvent =
+        Event(
+            eid = "13413555968",
+            title = "E2ECreateEvent",
+            description = "This event aims to test the creation of an event",
+            participants = 1,
+            maxParticipants = 2,
+            date = Timestamp(0, 0), // 01/01/1970 00:00
+            owner = "E2ECreateEventOwner")
+
+    composeTestRule.setContent {
+      StreetWorkApp(
+          parkLocationViewModel,
+          { navigateTo(currentScreen) },
+          { mapISLoaded = true },
+          userViewModel,
+          parkViewModel,
+          eventViewModel,
+          true)
+    }
+  }
+
+  /**
+   * This end to end test simulates a user flow where the user creates an event and then displays it
+   * in the park overview screen. We also verify that the event is properly displayed in the event
+   * overview screen. Warning: this test fails between 00:00 and 01:00 UTC time because of the date
+   * comparison
+   */
+  @Ignore("Test does not work on CI but should run locally")
+  @Test
+  fun e2eCanCreateEventAndDisplayIt() {
+    // Wait for the map to be loaded
+    composeTestRule.waitUntil(100000) { mapISLoaded }
+    composeTestRule.waitForIdle()
+
+    // test the map screen
+    composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("googleMap").assertIsDisplayed()
+
+    // find a marker on the map and click it
+    val uiDevice = UiDevice.getInstance(getInstrumentation())
+
+    composeTestRule.waitUntil {
+      uiDevice.findObject(UiSelector().descriptionContains("Marker1")).exists()
     }
 
-    /**
-     * This end to end test simulates a user flow where the user creates an event and then displays it
-     * in the park overview screen. We also verify that the event is properly displayed in the event
-     * overview screen.
-     */
-    @Ignore("test does not work on CI but works locally")
-    @Test
-    fun e2eCanCreateEventAndDisplayIt() {
-        // Wait for the map to be loaded
-        composeTestRule.waitUntil(100000) { mapISLoaded }
-        composeTestRule.waitForIdle()
+    val marker = uiDevice.findObject(UiSelector().descriptionContains("Marker1"))
 
-        // test the map screen
-        composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("googleMap").assertIsDisplayed()
-
-        // find a marker on the map and click it
-        val uiDevice = UiDevice.getInstance(getInstrumentation())
-
-        composeTestRule.waitUntil(10000) {
-            uiDevice.findObject(UiSelector().descriptionContains("Marker1")).exists()
-        }
-
-        val marker = uiDevice.findObject(UiSelector().descriptionContains("Marker1"))
-
-        try {
-            marker.click()
-        } catch (e: UiObjectNotFoundException) {
-            throw e
-        }
-
-        // UiDevice does not seem to navigate properly to the next screen, so we manually set the
-        // current screen to Park Overview
-        currentScreen = Screen.PARK_OVERVIEW
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule.onNodeWithTag("parkOverviewScreen").assertIsDisplayed()
-
-        // create an event
-        composeTestRule.onNodeWithTag("createEventButton").assertIsDisplayed().performClick()
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule.onNodeWithTag("addEventScreen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("titleTag").assertIsDisplayed().performTextClearance()
-        composeTestRule.onNodeWithTag("titleTag").performTextInput(testEvent.title)
-
-        composeTestRule.onNodeWithTag("descriptionTag").assertIsDisplayed().performTextClearance()
-        composeTestRule.onNodeWithTag("descriptionTag").performTextInput(testEvent.description)
-
-        composeTestRule.onNodeWithTag("dateIcon").assertIsDisplayed().performClick()
-        composeTestRule.onNodeWithTag("validateDate").assertIsDisplayed().performClick()
-        composeTestRule.onNodeWithTag("timeIcon").assertIsDisplayed().performClick()
-        composeTestRule.onNodeWithTag("validateTime").assertIsDisplayed().performClick()
-
-        composeTestRule.onNodeWithTag("addEventButton").assertIsDisplayed().performClick()
-
-        composeTestRule.waitForIdle()
-
-        // verify that the event is properly displayed on the park overview screen
-        composeTestRule.onNodeWithTag("parkOverviewScreen").assertIsDisplayed()
-
-        composeTestRule.waitUntil { composeTestRule.onNodeWithTag("eventButton").isDisplayed() }
-
-        composeTestRule.onNodeWithText(testEvent.title).assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("eventButton").assertIsDisplayed().performClick()
-
-        composeTestRule.waitForIdle()
-
-        // navigate to the event overview screen and verify that the event is properly displayed
-        composeTestRule.onNodeWithTag("eventOverviewScreen").assertIsDisplayed()
-
-        composeTestRule
-            .onNodeWithTag("eventTitle")
-            .assertIsDisplayed()
-            .assertTextContains(testEvent.title)
-
-        composeTestRule.onNodeWithTag("eventOwner").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("date").assertIsDisplayed()
-
-        composeTestRule
-            .onNodeWithTag("eventDescription")
-            .assertIsDisplayed()
-            .assertTextContains(testEvent.description)
-
-        composeTestRule
-            .onNodeWithTag("participants")
-            .assertIsDisplayed()
-            .assertTextContains("Participants: ${testEvent.participants}/${testEvent.maxParticipants}")
-
-        composeTestRule.onNodeWithTag("participantsTab").assertIsDisplayed().performClick()
-
-        composeTestRule.onNodeWithTag("participantsList").assertIsDisplayed()
+    try {
+      marker.click()
+    } catch (e: UiObjectNotFoundException) {
+      throw e
     }
+
+    // UiDevice does not seem to navigate properly to the next screen, so we manually set the
+    // current screen to Park Overview
+    currentScreen = Screen.PARK_OVERVIEW
+
+    composeTestRule.waitUntil(
+        10000) { // this value is arbitrary, we just don't want the test to completely halt. Might
+          // need to tune it for the CI
+          composeTestRule.onNodeWithTag("parkOverviewScreen").isDisplayed()
+        }
+
+    composeTestRule.onNodeWithTag("parkOverviewScreen").assertIsDisplayed()
+
+    // create an event
+    composeTestRule.onNodeWithTag("createEventButton").assertIsDisplayed().performClick()
+
+    composeTestRule.waitForIdle()
+
+    // I'm for real this does not work between 00:00 and 01:00 UTC time
+    composeTestRule.onNodeWithTag("addEventScreen").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("titleTag").assertIsDisplayed().performTextClearance()
+    composeTestRule.onNodeWithTag("titleTag").performTextInput(testEvent.title)
+
+    composeTestRule.onNodeWithTag("descriptionTag").assertIsDisplayed().performTextClearance()
+    composeTestRule.onNodeWithTag("descriptionTag").performTextInput(testEvent.description)
+
+    composeTestRule.onNodeWithTag("dateIcon").performClick()
+    composeTestRule.onNodeWithTag("validateDate").performClick()
+    composeTestRule.onNodeWithTag("timeIcon").performClick()
+    composeTestRule.onNodeWithTag("validateTime").performClick()
+
+    composeTestRule.onNodeWithTag("addEventButton").assertIsDisplayed().performClick()
+
+    composeTestRule.waitForIdle()
+
+    // verify that the event is properly displayed on the park overview screen
+    composeTestRule.onNodeWithTag("parkOverviewScreen").assertIsDisplayed()
+
+    composeTestRule.waitUntil { composeTestRule.onNodeWithTag("eventButton").isDisplayed() }
+
+    composeTestRule.onNodeWithText(testEvent.title).assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("eventButton").assertIsDisplayed().performClick()
+
+    composeTestRule.waitForIdle()
+
+    // navigate to the event overview screen and verify that the event is properly displayed
+    composeTestRule.onNodeWithTag("eventOverviewScreen").assertIsDisplayed()
+
+    composeTestRule
+        .onNodeWithTag("eventTitle")
+        .assertIsDisplayed()
+        .assertTextContains(testEvent.title)
+
+    composeTestRule.onNodeWithTag("eventOwner").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("date").assertIsDisplayed()
+
+    composeTestRule
+        .onNodeWithTag("eventDescription")
+        .assertIsDisplayed()
+        .assertTextContains(testEvent.description)
+
+    composeTestRule
+        .onNodeWithTag("participants")
+        .assertIsDisplayed()
+        .assertTextContains("Participants: ${testEvent.participants}/${testEvent.maxParticipants}")
+
+    composeTestRule.onNodeWithTag("participantsTab").assertIsDisplayed().performClick()
+
+    composeTestRule.onNodeWithTag("participantsList").assertIsDisplayed()
+  }
 }
