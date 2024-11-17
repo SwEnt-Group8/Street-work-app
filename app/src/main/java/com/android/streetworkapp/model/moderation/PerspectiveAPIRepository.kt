5e55e134e7f98de39c3daa5261eba90e9f9b15dd
Alex <alexandre.noren@hotmail.com>
2024-11-17 17:29:21 +0100

    feat: fix deserialization issues

--- app/src/main/java/com/android/streetworkapp/model/moderation/PerspectiveAPIRepository.kt
+++ app/src/main/java/com/android/streetworkapp/model/moderation/PerspectiveAPIRepository.kt
@@ -2,6 +2,7 @@ package com.android.streetworkapp.model.moderation
 
 import android.util.Log
 import androidx.compose.material3.Text
+import com.android.sample.BuildConfig
 import com.squareup.okhttp.RequestBody
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
@@ -19,7 +20,7 @@ import java.net.HttpURLConnection.HTTP_OK
 class PerspectiveAPIRepository(private val client: OkHttpClient): TextModerationRepository {
     private val DEBUG_PREFIX = "PerspectiveAPIRepository:"
 
-    override suspend fun evaluateText(content: String): TextEvaluationResult {
+    override fun evaluateText(content: String): TextEvaluationResult {
         //I don't perform any checks on the content, I'll let the api handle the error checking
 
         val requestMediaType = "application/json; charset=utf-8".toMediaType()
@@ -27,12 +28,13 @@ class PerspectiveAPIRepository(private val client: OkHttpClient): TextModeration
 
         //TODO: parse api key
         //note: I'm pretty sure someone could get the key by sniffing the packets, but since we can't have a backend here we are :)
-        val request = Request.Builder().url("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=").post(requestBody).build()
+        val request = Request.Builder().url("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${BuildConfig.PERSPECTIVE_API_KEY}").post(requestBody).build()
 
         val response = client.newCall(request).execute()
         when(response.code) {
             HTTP_OK -> {
-                val annotations = this.extractTagsAndProbabilitiesFromResponseBody(response.body.toString())
+                val responseBody = response.body?.string() ?: run { return TextEvaluationResult.Error(PerspectiveApiErrors.EMPTY_BODY_RESPONSE) }
+                val annotations = this.extractTagsAndProbabilitiesFromResponseBody(responseBody)
                 annotations?.let {
                     return TextEvaluationResult.Success(annotations)
                 } ?: run {
@@ -44,12 +46,13 @@ class PerspectiveAPIRepository(private val client: OkHttpClient): TextModeration
                 return TextEvaluationResult.Error(enumValues<PerspectiveApiErrors>().find { it.name == error.status  } ?: PerspectiveApiErrors.UNKNOWN_ERROR)
             }
             else -> {
-                Log.d(DEBUG_PREFIX, "Received unsupported http code (${response.code}) from api")
+                Log.d(this.DEBUG_PREFIX, "Received unsupported http code (${response.code}) from api")
                 return TextEvaluationResult.Error(PerspectiveApiErrors.UNSUPPORTED_HTTP_CODE)
             }
         }
     }
-    override fun formatPostRequestBody(content: String): String {
+
+    private fun formatPostRequestBody(content: String): String {
         val request = Request(
             comment = Comment(content),
             requestedAttributes = TEXT_MODERATION_TAGS.entries.associate { it.name to Unit },
@@ -60,10 +63,9 @@ class PerspectiveAPIRepository(private val client: OkHttpClient): TextModeration
         return Json.encodeToJsonElement(request).toString()
     }
 
-    override fun extractTagsAndProbabilitiesFromResponseBody(responseBody: String): List<TagAnnotation>? {
-        val attributeScoresMap = Json.decodeFromString<Map<String, AttributeScore>>(responseBody)
-
+    private fun extractTagsAndProbabilitiesFromResponseBody(responseBody: String): List<TagAnnotation>? {
         try {
+            val attributeScoresMap = Json.decodeFromString<SuccessResponse>(responseBody).attributeScores
             val tagsAnnotations = attributeScoresMap.map { (tag, attributeScore) ->
                 TagAnnotation(
                     enumValueOf<TEXT_MODERATION_TAGS>(tag),
@@ -73,7 +75,7 @@ class PerspectiveAPIRepository(private val client: OkHttpClient): TextModeration
 
             return tagsAnnotations
         } catch (e : Exception) {
-            Log.d("PerspectiveApiRepository:", "Failed to map response body into valid List<TagAnnotation>")
+            Log.d(this.DEBUG_PREFIX, "Failed to map response body into valid List<TagAnnotation>")
             return null
         }
     }
