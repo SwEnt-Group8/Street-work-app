4535e6c4291e61a6bec2a963dd3c5e27f6eef7be
Alex <alexandre.noren@hotmail.com>
2024-11-17 17:29:45 +0100

    refactor: remove unused function defs

--- app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationRepository.kt
+++ app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationRepository.kt
@@ -1,7 +1,5 @@
 package com.android.streetworkapp.model.moderation
 
 interface TextModerationRepository {
-    suspend fun evaluateText(content: String): TextEvaluationResult
-    fun formatPostRequestBody(content: String): String
-    fun extractTagsAndProbabilitiesFromResponseBody(responseBody: String): List<TagAnnotation>?
+    fun evaluateText(content: String): TextEvaluationResult
 }
