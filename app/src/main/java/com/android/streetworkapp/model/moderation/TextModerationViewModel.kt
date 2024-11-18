9ba949bf672b0525baf977f35aa9a27cf70ef8f0
Alex <alexandre.noren@hotmail.com>
2024-11-18 10:35:32 +0100

    test: implement TextModerationViewModel tests

--- app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationViewModel.kt
+++ app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class TextModerationViewModel(val repository: TextModerationRepository): ViewModel() {
-    fun analyzeText(content: String, onTextUnderThresholds: () -> Unit, onTextOverThresholds: () -> Unit, thresholds: Map<TextModerationTags, Double> = PerspectiveAPIThresholds.DEFAULT_THRESHOLD_VALUES): Boolean {
+open class TextModerationViewModel(val repository: TextModerationRepository): ViewModel() {
+    fun analyzeText(content: String, onTextUnderThresholds: () -> Unit, onTextOverThresholds: () -> Unit, thresholds: Map<TextModerationTags, Double> = PerspectiveAPIThresholds.DEFAULT_THRESHOLD_VALUES) {
         viewModelScope.launch {
             if (repository.evaluateText(content, thresholds))
                 onTextUnderThresholds()
