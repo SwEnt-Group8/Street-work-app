d5d64e080c87a1466e762d097d8fd41f5fd17c3a
Alex <alexandre.noren@hotmail.com>
2024-11-18 10:00:33 +0100

    feat: implement analyzeText in TextModerationViewModel

--- app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationViewModel.kt
+++ app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationViewModel.kt
@@ -1,13 +1,20 @@
 package com.android.streetworkapp.model.moderation
 
-class TextModerationViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
+class TextModerationViewModel(val repository: TextModerationRepository): ViewModel() {
+    fun analyzeText(content: String, onTextUnderThresholds: () -> Unit, onTextOverThresholds: () -> Unit, thresholds: Map<TextModerationTags, Double> = PerspectiveAPIThresholds.DEFAULT_THRESHOLD_VALUES): Boolean {
+        viewModelScope.launch {
+            if (repository.evaluateText(content, thresholds))
+                onTextUnderThresholds()
+            else
+                onTextOverThresholds()
+        }
+    }
 }
 
 
-
-data class TagAnnotation(val tag: TEXT_MODERATION_TAGS, val probability: Double)
-
-enum class TEXT_MODERATION_TAGS {
-    TOXICITY, INSULT, THREAT
-}
