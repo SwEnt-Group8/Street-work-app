260c55b93e49f95853fb08ba47e170dfc25e245f
Alex <alexandre.noren@hotmail.com>
2024-11-18 09:54:03 +0100

    refactor: added one extra function to the interface

--- app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationRepository.kt
+++ app/src/main/java/com/android/streetworkapp/model/moderation/TextModerationRepository.kt
@@ -1,5 +1,18 @@
 package com.android.streetworkapp.model.moderation
 
 interface TextModerationRepository {
-    fun evaluateText(content: String): TextEvaluationResult
+    /**
+     * Analyses the content, returns true if the content is under the threshold, false otherwise
+     * @param content The text to be analyzed
+     * @param thresholds A map that holds the threshold values for each tags. Each threshold is the probability of the text being included in this tag, ranges from 0-1.
+     * @return True if the text is under the thresholds for all tags, false otherwise
+     */
+    fun evaluateText(content: String, thresholds: Map<TextModerationTags, Double>): Boolean
+
+    /**
+     * Gets the tags and their probabilities for param content
+     * @param content Text to be analyzed
+     * @return TextEvaluationResult.Success if the API could process the content, TextEvaluationResult.Error if an error was encountered
+     */
+    fun getTextAnnotations(content: String): TextEvaluationResult
 }
