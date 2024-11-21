1039394e971d5d9623564fac0aa0048b40c0a934
tercierp <paul.tercier@epfl.ch>
2024-11-21 14:42:38 +0100

    test(workout): add the tests for WorkoutDataTest

--- app/src/test/java/com/android/streetworkapp/model/workout/WorkoutDataTest.kt
+++ app/src/test/java/com/android/streetworkapp/model/workout/WorkoutDataTest.kt
@@ -6,28 +6,31 @@ import org.junit.Test
 
 class WorkoutDataTest {
 
-    @Test
-    fun `test WorkoutData default values`() {
-        // Create an instance of WorkoutData with default values
-        val workoutData = WorkoutData()
+  @Test
+  fun `test WorkoutData default values`() {
+    // Create an instance of WorkoutData with default values
+    val workoutData = WorkoutData()
 
-        // Assert default values
-        assertEquals("", workoutData.userUid)
-        assertTrue(workoutData.workoutSessions.isEmpty())
-    }
+    // Assert default values
+    assertEquals("", workoutData.userUid)
+    assertTrue(workoutData.workoutSessions.isEmpty())
+  }
 
-    @Test
-    fun `test WorkoutData with custom values`() {
-        // Prepare custom values
-        val workoutSessions = listOf(WorkoutSession("session1", 0L, 0L, SessionType.ALONE), WorkoutSession("session2", 0L, 0L, SessionType.ALONE))
-        val userUid = "test_user"
+  @Test
+  fun `test WorkoutData with custom values`() {
+    // Prepare custom values
+    val workoutSessions =
+        listOf(
+            WorkoutSession("session1", 0L, 0L, SessionType.ALONE),
+            WorkoutSession("session2", 0L, 0L, SessionType.ALONE))
+    val userUid = "test_user"
 
-        // Create an instance of WorkoutData with custom values
-        val workoutData = WorkoutData(userUid = userUid, workoutSessions = workoutSessions)
+    // Create an instance of WorkoutData with custom values
+    val workoutData = WorkoutData(userUid = userUid, workoutSessions = workoutSessions)
 
-        // Assert custom values
-        assertEquals("test_user", workoutData.userUid)
-        assertEquals(2, workoutData.workoutSessions.size)
-        assertEquals("session1", workoutData.workoutSessions[0].sessionId)
-    }
+    // Assert custom values
+    assertEquals("test_user", workoutData.userUid)
+    assertEquals(2, workoutData.workoutSessions.size)
+    assertEquals("session1", workoutData.workoutSessions[0].sessionId)
+  }
 }
