7792a96135b04be04005dfe6c0bf2ad20520088c
tercierp <paul.tercier@epfl.ch>
2024-11-23 21:43:11 +0100

    chore(format): apply ktfmt formatting

--- app/src/main/java/com/android/streetworkapp/utils/GraphData.kt
+++ app/src/main/java/com/android/streetworkapp/utils/GraphData.kt
@@ -38,122 +38,115 @@ class GraphConfiguration(
 @SuppressLint("DefaultLocale")
 @Composable
 fun Graph(modifier: Modifier = Modifier, graphConfiguration: GraphConfiguration) {
-    val graphColor = graphConfiguration.graphColor
-    val axisColor = graphConfiguration.axisColor
-    val dataPoints = graphConfiguration.dataPoints
-    val xUnitLabel = graphConfiguration.xUnitLabel
-    val yUnitLabel = graphConfiguration.yUnitLabel
-    val strokeWidth = graphConfiguration.strokeWidth
+  val graphColor = graphConfiguration.graphColor
+  val axisColor = graphConfiguration.axisColor
+  val dataPoints = graphConfiguration.dataPoints
+  val xUnitLabel = graphConfiguration.xUnitLabel
+  val yUnitLabel = graphConfiguration.yUnitLabel
+  val strokeWidth = graphConfiguration.strokeWidth
 
-    var hoverPosition by remember { mutableStateOf<Offset?>(null) }
-    var hoverValue by remember { mutableStateOf<String?>(null) }
+  var hoverPosition by remember { mutableStateOf<Offset?>(null) }
+  var hoverValue by remember { mutableStateOf<String?>(null) }
 
-    val density = LocalDensity.current
+  val density = LocalDensity.current
 
-    if (dataPoints.isNotEmpty()) {
-        val xValues = dataPoints.map { it.x }
-        val yValues = dataPoints.map { it.y }
-        val xMin = xValues.minOrNull() ?: 0f
-        val xMax = xValues.maxOrNull() ?: 1f
-        val yMin = yValues.minOrNull() ?: 0f
-        val yMax = yValues.maxOrNull() ?: 1f
+  if (dataPoints.isNotEmpty()) {
+    val xValues = dataPoints.map { it.x }
+    val yValues = dataPoints.map { it.y }
+    val xMin = xValues.minOrNull() ?: 0f
+    val xMax = xValues.maxOrNull() ?: 1f
+    val yMin = yValues.minOrNull() ?: 0f
+    val yMax = yValues.maxOrNull() ?: 1f
 
-        Box(modifier = modifier.padding(16.dp)) {
-            // Add X-axis and Y-axis labels as Text Composables
-            Text(
-                text = xUnitLabel,
-                modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 8.dp),
-                color = Color.Black
-            )
-            Text(
-                text = yUnitLabel,
-                modifier = Modifier.align(Alignment.CenterStart).padding(start = 8.dp),
-                color = Color.Black
-            )
+    Box(modifier = modifier.padding(16.dp)) {
+      // Add X-axis and Y-axis labels as Text Composables
+      Text(
+          text = xUnitLabel,
+          modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 8.dp),
+          color = Color.Black)
+      Text(
+          text = yUnitLabel,
+          modifier = Modifier.align(Alignment.CenterStart).padding(start = 8.dp),
+          color = Color.Black)
 
-            Canvas(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .pointerInput(Unit) {
-                        detectTapGestures { offset ->
-                            val graphWidth = with(density) { size.width - 32.dp.toPx() }
-                            val graphHeight = with(density) { size.height - 32.dp.toPx() }
+      Canvas(
+          modifier =
+              Modifier.fillMaxSize().pointerInput(Unit) {
+                detectTapGestures { offset ->
+                  val graphWidth = with(density) { size.width - 32.dp.toPx() }
+                  val graphHeight = with(density) { size.height - 32.dp.toPx() }
 
-                            // Find the closest point
-                            val closestPoint = dataPoints.minByOrNull {
-                                val x =
-                                    ((it.x - xMin) / (xMax - xMin)) * graphWidth +
-                                            with(density) { 16.dp.toPx() }
-                                val y =
-                                    graphHeight + with(density) { 16.dp.toPx() } -
-                                            ((it.y - yMin) / (yMax - yMin)) * graphHeight
-                                abs(x - offset.x) + abs(y - offset.y)
-                            }
+                  // Find the closest point
+                  val closestPoint =
+                      dataPoints.minByOrNull {
+                        val x =
+                            ((it.x - xMin) / (xMax - xMin)) * graphWidth +
+                                with(density) { 16.dp.toPx() }
+                        val y =
+                            graphHeight + with(density) { 16.dp.toPx() } -
+                                ((it.y - yMin) / (yMax - yMin)) * graphHeight
+                        abs(x - offset.x) + abs(y - offset.y)
+                      }
 
-                            hoverPosition = closestPoint?.let {
-                                Offset(
-                                    ((it.x - xMin) / (xMax - xMin)) * graphWidth +
-                                            with(density) { 16.dp.toPx() },
-                                    graphHeight + with(density) { 16.dp.toPx() } -
-                                            ((it.y - yMin) / (yMax - yMin)) * graphHeight
-                                )
-                            }
-                            hoverValue = closestPoint?.let { point ->
-                                "(${String.format("%.1f", point.x)}, ${String.format("%.1f", point.y)})"
-                            }
-                        }
-                    }
-            ) {
-                val width = with(density) { size.width - 32.dp.toPx() }
-                val height = with(density) { size.height - 32.dp.toPx() }
-
-                val xOffset = with(density) { 16.dp.toPx() }
-                val yOffset = with(density) { 16.dp.toPx() }
-
-                // Draw X and Y axes
-                drawLine(
-                    color = axisColor,
-                    start = Offset(xOffset, height + yOffset),
-                    end = Offset(width + xOffset, height + yOffset),
-                    strokeWidth = with(density) { 2.dp.toPx() }
-                )
-                drawLine(
-                    color = axisColor,
-                    start = Offset(xOffset, height + yOffset),
-                    end = Offset(xOffset, yOffset),
-                    strokeWidth = with(density) { 2.dp.toPx() }
-                )
-
-                // Draw data lines
-                for (i in 1 until dataPoints.size) {
-                    val start = dataPoints[i - 1]
-                    val end = dataPoints[i]
-
-                    val startX = ((start.x - xMin) / (xMax - xMin)) * width + xOffset
-                    val startY = height + yOffset - ((start.y - yMin) / (yMax - yMin)) * height
-
-                    val endX = ((end.x - xMin) / (xMax - xMin)) * width + xOffset
-                    val endY = height + yOffset - ((end.y - yMin) / (yMax - yMin)) * height
-
-                    drawLine(
-                        color = graphColor,
-                        start = Offset(startX, startY),
-                        end = Offset(endX, endY),
-                        strokeWidth = with(density) { strokeWidth.dp.toPx() },
-                        cap = StrokeCap.Round
-                    )
+                  hoverPosition =
+                      closestPoint?.let {
+                        Offset(
+                            ((it.x - xMin) / (xMax - xMin)) * graphWidth +
+                                with(density) { 16.dp.toPx() },
+                            graphHeight + with(density) { 16.dp.toPx() } -
+                                ((it.y - yMin) / (yMax - yMin)) * graphHeight)
+                      }
+                  hoverValue =
+                      closestPoint?.let { point ->
+                        "(${String.format("%.1f", point.x)}, ${String.format("%.1f", point.y)})"
+                      }
                 }
+              }) {
+            val width = with(density) { size.width - 32.dp.toPx() }
+            val height = with(density) { size.height - 32.dp.toPx() }
 
-                // Draw hover position
-                hoverPosition?.let { position ->
-                    drawCircle(
-                        color = INTERACTION_COLOR_DARK,
-                        radius = with(density) { 5.dp.toPx() },
-                        center = position
-                    )
-                }
+            val xOffset = with(density) { 16.dp.toPx() }
+            val yOffset = with(density) { 16.dp.toPx() }
+
+            // Draw X and Y axes
+            drawLine(
+                color = axisColor,
+                start = Offset(xOffset, height + yOffset),
+                end = Offset(width + xOffset, height + yOffset),
+                strokeWidth = with(density) { 2.dp.toPx() })
+            drawLine(
+                color = axisColor,
+                start = Offset(xOffset, height + yOffset),
+                end = Offset(xOffset, yOffset),
+                strokeWidth = with(density) { 2.dp.toPx() })
+
+            // Draw data lines
+            for (i in 1 until dataPoints.size) {
+              val start = dataPoints[i - 1]
+              val end = dataPoints[i]
+
+              val startX = ((start.x - xMin) / (xMax - xMin)) * width + xOffset
+              val startY = height + yOffset - ((start.y - yMin) / (yMax - yMin)) * height
+
+              val endX = ((end.x - xMin) / (xMax - xMin)) * width + xOffset
+              val endY = height + yOffset - ((end.y - yMin) / (yMax - yMin)) * height
+
+              drawLine(
+                  color = graphColor,
+                  start = Offset(startX, startY),
+                  end = Offset(endX, endY),
+                  strokeWidth = with(density) { strokeWidth.dp.toPx() },
+                  cap = StrokeCap.Round)
             }
-        }
-    }
-}
 
+            // Draw hover position
+            hoverPosition?.let { position ->
+              drawCircle(
+                  color = INTERACTION_COLOR_DARK,
+                  radius = with(density) { 5.dp.toPx() },
+                  center = position)
+            }
+          }
+    }
+  }
+}
--- app/src/test/java/com/android/streetworkapp/utils/GraphDataTest.kt
+++ app/src/test/java/com/android/streetworkapp/utils/GraphDataTest.kt
@@ -17,34 +17,32 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricTestRunner::class)
 class GraphTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @Before
-    fun setUp() {
-        System.setProperty("ro.build.fingerprint", "test_fingerprint")
-        println("Mocked ro.build.fingerprint: ${System.getProperty("ro.build.fingerprint")}")
-    }
+  @Before
+  fun setUp() {
+    System.setProperty("ro.build.fingerprint", "test_fingerprint")
+    println("Mocked ro.build.fingerprint: ${System.getProperty("ro.build.fingerprint")}")
+  }
 
-    @Test
-    fun testGraphRendersAxesAndLabels() {
-        val configuration = GraphConfiguration(
+  @Test
+  fun testGraphRendersAxesAndLabels() {
+    val configuration =
+        GraphConfiguration(
             graphColor = Color.Blue,
             axisColor = Color.Black,
             xUnitLabel = "Time",
             yUnitLabel = "Reps",
-            dataPoints = listOf(GraphData(0f, 0f), GraphData(1f, 1f))
-        )
+            dataPoints = listOf(GraphData(0f, 0f), GraphData(1f, 1f)))
 
-        composeTestRule.setContent {
-            Graph(modifier = Modifier.fillMaxSize(), graphConfiguration = configuration)
-        }
-
-        // Assert X-axis label
-        composeTestRule.onNodeWithText("Time").assertExists()
-
-        // Assert Y-axis label
-        composeTestRule.onNodeWithText("Reps").assertExists()
+    composeTestRule.setContent {
+      Graph(modifier = Modifier.fillMaxSize(), graphConfiguration = configuration)
     }
 
+    // Assert X-axis label
+    composeTestRule.onNodeWithText("Time").assertExists()
+
+    // Assert Y-axis label
+    composeTestRule.onNodeWithText("Reps").assertExists()
+  }
 }
