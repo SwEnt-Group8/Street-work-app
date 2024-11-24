de72e5b9c0129ea6a71158c6f0a9fd5b85bd47e7
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-11-24 16:46:28 +0100

    fix: apply ktfmtFormat

--- app/src/test/java/com/android/streetworkapp/model/progression/ProgressionModelTest.kt
+++ app/src/test/java/com/android/streetworkapp/model/progression/ProgressionModelTest.kt
@@ -1,16 +1,14 @@
 package com.android.streetworkapp.model.progression
 
 import kotlinx.coroutines.test.runTest
-
 import org.junit.Test
-
 
 class ProgressionModelTest {
 
-    @Test
-    fun getOrAddProgressionCallsRepository() = runTest {
-        assert(ScoreIncrease.CREATE_EVENT.scoreAdded == 30)
-        assert(ScoreIncrease.JOIN_EVENT.scoreAdded == 60)
-        assert(ScoreIncrease.ADD_FRIEND.scoreAdded == 90)
-    }
+  @Test
+  fun getOrAddProgressionCallsRepository() = runTest {
+    assert(ScoreIncrease.CREATE_EVENT.scoreAdded == 30)
+    assert(ScoreIncrease.JOIN_EVENT.scoreAdded == 60)
+    assert(ScoreIncrease.ADD_FRIEND.scoreAdded == 90)
+  }
 }
