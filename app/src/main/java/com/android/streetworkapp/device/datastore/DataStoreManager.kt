88913121781fe6512d278db6a331380283e659c1
Arthur Muster <arthur@must-r.com>
2024-11-26 11:13:04 +0100

    feat(data): add getter flow for preferences variables

--- app/src/main/java/com/android/streetworkapp/device/datastore/DataStoreManager.kt
+++ app/src/main/java/com/android/streetworkapp/device/datastore/DataStoreManager.kt
@@ -1,12 +1,18 @@
 package com.android.streetworkapp.device.datastore
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import java.io.IOException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 
 class DataStoreManager(context: Context) {
 
@@ -18,6 +24,34 @@ class DataStoreManager(context: Context) {
     val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
     val SAVED_UID = stringPreferencesKey("saved_uid")
   }
+
+  /** A flow that emits the login state of the user. */
+  val isLoggedInFlow: Flow<Boolean> =
+      dataStore.data
+          .catch { exception ->
+            // Handle exceptions
+            if (exception is IOException) {
+              emit(emptyPreferences())
+              Log.d("DataStoreManager", "An IO exception occurred, emitting empty preferences.")
+            } else {
+              Log.d("DataStoreManager", exception.message.toString())
+            }
+          }
+          .map { preferences -> preferences[IS_LOGGED_IN] ?: false }
+
+  /** A flow that emits the user's uid. */
+  val savedUidFlow: Flow<String> =
+      dataStore.data
+          .catch { exception ->
+            // Handle exceptions
+            if (exception is IOException) {
+              emit(emptyPreferences())
+              Log.d("DataStoreManager", "An IO exception occurred, emitting empty preferences.")
+            } else {
+              Log.d("DataStoreManager", exception.message.toString())
+            }
+          }
+          .map { preferences -> preferences[SAVED_UID] ?: "" }
 
   /**
    * Saves the login state of the user in preferences.
