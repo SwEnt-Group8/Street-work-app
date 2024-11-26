9cbb407d7834bc4fd7868e49043cb37883dadbfe
Arthur Muster <arthur@must-r.com>
2024-11-26 11:03:04 +0100

    feat(data): add preference variables to `DataStoreManager`

--- app/src/main/java/com/android/streetworkapp/device/datastore/DataStoreManager.kt
+++ app/src/main/java/com/android/streetworkapp/device/datastore/DataStoreManager.kt
@@ -1,3 +1,19 @@
 package com.android.streetworkapp.device.datastore
 
-class DataStoreManager {}
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+class DataStoreManager(context: Context) {
+
+  private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+  private val dataStore = context.dataStore
+
+  companion object {
+    val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
+    val SAVED_UID = stringPreferencesKey("saved_uid")
+  }
+}
