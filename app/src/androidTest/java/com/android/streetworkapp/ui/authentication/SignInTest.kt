15769071926f47ce18dc57a78839be0939fd4d57
Kemin <kemin.zhengepfl.ch>
2024-11-29 02:15:07 +0100

    fix(SigInTest): print for CI test

--- app/src/androidTest/java/com/android/streetworkapp/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/authentication/SignInTest.kt
@@ -37,44 +37,62 @@ class LoginTest : TestCase() {
 
     // For Box, Text, Image, Buttons, List : check if displayed :
     composeTestRule.onNodeWithTag("loginScreenBoxContainer").assertExists().assertIsDisplayed()
+    print("loginScreenBoxContainer")
     composeTestRule.onNodeWithTag("loginScreenAppLogo").assertExists().assertIsDisplayed()
+    print("loginScreenAppLogo")
     composeTestRule.onNodeWithTag("loginScreenFirstRowIcon").assertExists().assertIsDisplayed()
+    print("loginScreenFirstRowIcon")
     composeTestRule.onNodeWithTag("loginButton").assertExists().assertIsDisplayed()
+    print("loginButton")
     composeTestRule
         .onNodeWithTag("loginButtonIcon", useUnmergedTree = true)
         .assertExists()
         .assertIsDisplayed()
+    print("loginButtonIcon")
 
     // For Pager component
     composeTestRule.onNodeWithTag("introScreenBoxContainer").assertExists().assertIsDisplayed()
+    print("introScreenBoxContainer")
     composeTestRule.onNodeWithTag("introBox1").assertExists().assertIsDisplayed()
+    print("introBox1")
     composeTestRule.onNodeWithTag("introImage1").assertExists().assertIsDisplayed()
+    print("introImage1")
     composeTestRule.onNodeWithTag("introApp1").assertExists().assertIsDisplayed()
+    print("introApp1")
     composeTestRule.onNodeWithTag("introDotRow").assertExists().assertIsDisplayed()
+    print("introDotRow")
     composeTestRule.onNodeWithTag("introColumn1").assertExists().assertIsDisplayed()
+    print("introColumn1")
 
     composeTestRule.onNodeWithTag("loginButtonText", useUnmergedTree = true).assertIsDisplayed()
-
+    print("loginButtonText")
     // For columns / rows / spacers : check if exist :
     composeTestRule.onNodeWithTag("loginScreenColumnContainer").assertExists()
+    print("loginScreenColumnContainer")
     composeTestRule.onNodeWithTag("loginScreenFirstSpacer").assertExists()
+    print("loginScreenFirstSpacer")
     composeTestRule.onNodeWithTag("loginScreenFirstRow").assertExists().assertIsDisplayed()
+    print("loginScreenFirstRow")
     composeTestRule.onNodeWithTag("loginScreenFirstRowSpacer").assertExists()
+    print("loginScreenFirstRowSpacer")
     composeTestRule.onNodeWithTag("loginButtonRowContainer", useUnmergedTree = true).assertExists()
-
+    print("loginButtonRowContainer")
     // UX elements :
 
     // UX - Text values :
     composeTestRule
         .onNodeWithTag("loginScreenFirstRowText")
         .assertTextEquals("Find nearby parks and events to participate in or create")
+    print("loginScreenFirstRowText")
     composeTestRule.onNodeWithTag("loginButton").assertTextEquals("Sign in with Google")
+    print("loginButton")
     composeTestRule
         .onNodeWithTag("loginButtonText", useUnmergedTree = true)
         .assertTextEquals("Sign in with Google")
-
+    print("loginButtonText")
     // UX - Button click action :
     composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
+    print("loginButton")
   }
 
   @Test
