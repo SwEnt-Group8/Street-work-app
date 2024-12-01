3a368c1884371ea27d4e50857bde1093c1f78b3b
Alex <alexandre.noren@hotmail.com>
2024-12-01 16:23:02 +0100

    feat: add bottom warning message and change text button to IconButton

--- app/src/main/java/com/android/streetworkapp/ui/image/ConfirmImageDialog.kt
+++ app/src/main/java/com/android/streetworkapp/ui/image/ConfirmImageDialog.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +16,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +40,9 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -44,6 +54,7 @@ import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import coil.util.DebugLogger
 import com.android.sample.R
+import com.android.streetworkapp.model.park.Park
 import com.android.streetworkapp.ui.theme.ColorPalette
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import java.io.File
@@ -71,41 +82,71 @@ fun ConfirmImageDialog(
                 modifier = Modifier.padding(16.dp)
             ) {
                 Text(
-                    text = "Confirm Image",
+                    text = "Add this Photo to the Park?",
                     fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
+                    color = ColorPalette.PRIMARY_TEXT_COLOR,
+                    fontWeight = FontWeight.SemiBold,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
 
-                    imageBitmap?.let {
-                        Image(
-                            bitmap = it.asImageBitmap(),
-                            contentDescription = "Taken camera picture",
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                imageBitmap?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = "Taken camera picture",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Text(
+                    text= "Note: Your photo will be linked to your account and visible to anyone using the app.",
+                    color = ColorPalette.SECONDARY_TEXT_COLOR,
+                    fontSize = 13.sp,
+                    textAlign = TextAlign.Center
+                    )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Box(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        // Cancel icon button
+                        IconButton(
+                            onClick = { onCancel() },
+                            modifier = Modifier.background(
+                                color = MaterialTheme.colorScheme.error,
+                                shape = CircleShape
+                            )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "Cancel",
+                                tint = Color.White
+                            )
+                        }
                     }
 
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        // Cancel button
-                        Button(
-                            onClick = { onCancel() },
-                            colors = ColorPalette.BUTTON_COLOR
+                    Box(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        // Confirm (upload) icon button
+                        IconButton(
+                            onClick = { onConfirm() },
+                            modifier = Modifier.background(
+                                color = ColorPalette.INTERACTION_COLOR_DARK,
+                                shape = CircleShape
+                            )
                         ) {
-                            Text(text = "Cancel", color = Color.Black)
-                        }
-                        // Confirm button
-                        Button(
-                            onClick = { onConfirm() }
-                        ) {
-                            Text(text = "Upload")
+                            Icon(
+                                imageVector = Icons.Default.Upload,
+                                contentDescription = "Upload",
+                                tint = Color.White
+                            )
                         }
                     }
                 }
             }
         }
     }
+}
