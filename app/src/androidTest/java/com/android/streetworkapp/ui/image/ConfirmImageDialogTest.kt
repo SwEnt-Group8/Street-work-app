db4c473df09b61054d129a5352f58d6f50c95e79
Alex <alexandre.noren@hotmail.com>
2024-12-02 10:10:28 +0100

    test: change componentsExists to componentsAreDisplayedCorrectly

--- app/src/androidTest/java/com/android/streetworkapp/ui/image/ConfirmImageDialogTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/image/ConfirmImageDialogTest.kt
@@ -1,35 +1,22 @@
 package com.android.streetworkapp.ui.image
 
-import android.content.ContentResolver
-import android.content.Context
-import android.net.Uri
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.core.net.toUri
-import androidx.test.core.app.ApplicationProvider
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.verify
 
 class ConfirmImageDialogTest {
-
-  @Mock private lateinit var mockContext: Context
-  @Mock private lateinit var mockContentResolver: ContentResolver
-  @Mock private lateinit var mockUri: Uri
-  @Mock private lateinit var mockInputStream: InputStream
-
   private lateinit var testImageFile: File
-
-  private lateinit var context: Context
 
   @get:Rule val temporaryFolder = TemporaryFolder()
 
@@ -38,7 +25,6 @@ class ConfirmImageDialogTest {
   @Before
   fun setUp() {
     MockitoAnnotations.openMocks(this)
-    context = ApplicationProvider.getApplicationContext()
 
     testImageFile = temporaryFolder.newFile("test_confirm_image_dialog.jpg")
     FileOutputStream(testImageFile).use { outputStream ->
@@ -48,20 +34,17 @@ class ConfirmImageDialogTest {
   }
 
   // Note: as the image is an AsyncImage, I won't test it
-  // I only check if they exists, for some reason in the debugging there is some weird issue where
-  // the components aren't 'displayed'
   @Test
-  fun componentsExists() {
+  fun componentsAreDisplayedCorrectly() {
     composeTestRule.setContent {
-      ConfirmImageDialog(
-          imageUri = testImageFile.toUri(), onConfirm = {}, onCancel = {}, context = context)
+      ConfirmImageDialog(imageUri = testImageFile.toUri(), onConfirm = {}, onCancel = {})
     }
 
     composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag("dialogTitle").assertExists()
-    composeTestRule.onNodeWithTag("disclaimerText").assertExists()
-    composeTestRule.onNodeWithTag("cancelButton").assertExists()
-    composeTestRule.onNodeWithTag("uploadButton").assertExists()
+    composeTestRule.onNodeWithTag("dialogTitle").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("disclaimerText").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("cancelButton").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("uploadButton").assertIsDisplayed()
   }
 
   @Test
@@ -73,8 +56,7 @@ class ConfirmImageDialogTest {
       ConfirmImageDialog(
           imageUri = testImageFile.toUri(),
           onConfirm = { onConfirm.run() },
-          onCancel = { onCancel.run() },
-          context = context)
+          onCancel = { onCancel.run() })
     }
 
     composeTestRule.waitForIdle()
