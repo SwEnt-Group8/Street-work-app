4a8fa5421153dbbf9d0afc0fc5ef0666a3f4453d
Arthur Muster <arthur@must-r.com>
2024-12-03 16:37:25 +0100

    feat(ui): add a splash screen composable

---
+++ app/src/main/java/com/android/streetworkapp/ui/miscellaneous/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.android.streetworkapp.ui.miscellaneous
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+/** Display a splash screen. */
+@Composable
+fun SplashScreen() {
+  Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    CircularProgressIndicator()
+  }
+}
