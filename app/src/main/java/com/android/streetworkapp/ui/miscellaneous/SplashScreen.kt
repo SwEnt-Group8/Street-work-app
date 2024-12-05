ae21f8659350ed6cba93aed5aa9755c8ace4bb30
Arthur Muster <arthur@must-r.com>
2024-12-05 12:58:35 +0100

    feat(ui): add test tags to SplashScreen

--- app/src/main/java/com/android/streetworkapp/ui/miscellaneous/SplashScreen.kt
+++ app/src/main/java/com/android/streetworkapp/ui/miscellaneous/SplashScreen.kt
@@ -6,11 +6,15 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 /** Display a splash screen. */
 @Composable
 fun SplashScreen() {
-  Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-    CircularProgressIndicator()
-  }
+  Box(
+      modifier = Modifier.fillMaxSize().testTag("SplashScreen"),
+      contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(
+            modifier = Modifier.testTag("SplashScreenCircularProgressIndicator"))
+      }
 }
