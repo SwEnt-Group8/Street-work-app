2fae9b5614550298fcb39e6b76fb34321451c3fb
tercierp <paul.tercier@epfl.ch>
2024-12-05 16:22:20 +0100

    chore(format): apply ktfmt formatting

--- app/src/test/java/com/android/streetworkapp/model/workout/WorkoutRepositoryFirestoreTest.kt
+++ app/src/test/java/com/android/streetworkapp/model/workout/WorkoutRepositoryFirestoreTest.kt
@@ -179,31 +179,30 @@ class WorkoutRepositoryFirestoreTest {
     val expectedFieldPath = "workoutSessions.$sessionId.exercises.$exerciseIndex"
     assertEquals(updatedExercise, capturedUpdate[expectedFieldPath])
   }
+
   @Test
   fun saveWorkoutDataSavesDataToFirestore() = runTest {
     val uid = "testUid"
-    val workoutData = WorkoutData(
-      userUid = uid,
-      workoutSessions = listOf(
-        WorkoutSession(
-          sessionId = "sessionId1",
-          startTime = 123456789L,
-          endTime = 123456999L,
-          sessionType = SessionType.SOLO,
-          participants = listOf("user1", "user2"),
-          exercises = listOf(
-            Exercise(
-              name = "Push-up",
-              reps = 10,
-              sets = 3,
-              weight = 20f,
-              duration = 30
-            )
-          ),
-          winner = null
-        )
-      )
-    )
+    val workoutData =
+        WorkoutData(
+            userUid = uid,
+            workoutSessions =
+                listOf(
+                    WorkoutSession(
+                        sessionId = "sessionId1",
+                        startTime = 123456789L,
+                        endTime = 123456999L,
+                        sessionType = SessionType.SOLO,
+                        participants = listOf("user1", "user2"),
+                        exercises =
+                            listOf(
+                                Exercise(
+                                    name = "Push-up",
+                                    reps = 10,
+                                    sets = 3,
+                                    weight = 20f,
+                                    duration = 30)),
+                        winner = null)))
 
     // Mock Firestore interactions
     `when`(db.collection(any())).thenReturn(collection)
