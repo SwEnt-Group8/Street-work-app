15b8562b104595249d3995d411025dfd14ec1d8d
Arthur Muster <arthur@must-r.com>
2024-12-08 21:14:36 +0100

    feat(test): add a logout button click test

--- app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
@@ -18,6 +18,7 @@ import com.android.streetworkapp.model.user.User
 import com.android.streetworkapp.model.user.UserRepository
 import com.android.streetworkapp.model.user.UserViewModel
 import com.android.streetworkapp.ui.navigation.NavigationActions
+import com.android.streetworkapp.ui.navigation.Route
 import com.android.streetworkapp.utils.GoogleAuthService
 import com.google.firebase.auth.FirebaseAuth
 import org.junit.Rule
@@ -25,6 +26,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.RETURNS_DEFAULTS
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 class SettingsTest {
@@ -97,5 +99,26 @@ class SettingsTest {
     composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("deleteAccountDialog").assertIsDisplayed()
+  }
+
+  @Test
+  fun testLogoutButtonNavigatesToAuthScreen() {
+    val showSettingDialog = mutableStateOf(false)
+    var context: Context? = null
+    val alice = User("uid-alice", "Alice", "alice@gmail.com", 42, emptyList(), "")
+    userViewModel.setCurrentUser(alice)
+
+    composeTestRule.setContent {
+      SettingsContent(
+          navigationActions, userViewModel, preferencesViewModel, authService, showSettingDialog)
+      context = LocalContext.current
+    }
+
+    // Click the logout button
+    composeTestRule.onNodeWithTag("LogOutButton").performClick()
+    composeTestRule.waitForIdle()
+
+    // Verify that the navigation action to the AUTH screen was called
+    verify(navigationActions).navigateTo(Route.AUTH)
   }
 }
