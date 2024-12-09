fb09efa272732621180a7390a09b03206313d84a
Alex <alexandre.noren@hotmail.com>
2024-12-09 17:17:19 +0100

    ui: first draft for complete collection image ui

--- app/src/main/java/com/android/streetworkapp/ui/image/FullScreenImagePopup.kt
+++ app/src/main/java/com/android/streetworkapp/ui/image/FullScreenImagePopup.kt
@@ -1,36 +1,56 @@
 package com.android.streetworkapp.ui.image
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ThumbDown
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.outlined.ThumbDown
+import androidx.compose.material.icons.outlined.ThumbUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import coil.compose.AsyncImage
 import com.android.streetworkapp.model.image.ParkImageLocal
+import com.android.streetworkapp.ui.theme.ColorPalette
 
 /** Fullscreen PopUp: it will display all the ParKImageLocal in the list in a HorizontalPager */
+@SuppressLint("AutoboxingStateCreation")
 @Composable
 fun FullScreenImagePopup(images: List<ParkImageLocal>, onDismiss: () -> Unit) {
+  val imagesSize by remember { mutableIntStateOf(images.size) }
   val imageUris by remember { mutableStateOf(images.map { it.image }) }
   // State for the pager to keep track of the current image
   val pagerState = rememberPagerState(pageCount = { imageUris.size })
@@ -41,31 +61,77 @@ fun FullScreenImagePopup(images: List<ParkImageLocal>, onDismiss: () -> Unit) {
           DialogProperties(
               usePlatformDefaultWidth = false // Allow the dialog to take full screen width
               )) {
-        Box(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier =
                 Modifier.fillMaxSize()
                     .background(Color.Black.copy(alpha = 0.80f))
+                    .verticalScroll(rememberScrollState())
                     .testTag("fullscreenImagePopUp")) {
-              // HorizontalPager to swipe between images
+              Box(modifier = Modifier.fillMaxWidth()) {
+                IconButton(
+                    onClick = onDismiss,
+                    modifier =
+                        Modifier.size(60.dp)
+                            .align(Alignment.TopEnd)
+                            .testTag("fullscreenImagePopUpCloseButton")) {
+                      Icon(
+                          imageVector = Icons.Default.Close,
+                          contentDescription = "Close",
+                          tint = Color.White)
+                    }
+              }
+
+            Text(" #${pagerState.currentPage + 1}/${imagesSize}", color= Color.White, fontWeight = FontWeight.SemiBold, fontSize = 18.sp)
+            // HorizontalPager to swipe between images
               HorizontalPager(
                   state = pagerState,
-                  modifier = Modifier.fillMaxWidth().fillMaxHeight(0.7f).align(Alignment.Center)) {
-                      page ->
-                    ImageItem(imageUri = imageUris[page])
+                  modifier =
+                      Modifier.fillMaxWidth().fillMaxHeight(0.7f).padding(vertical = 20.dp)) { page
+                    ->
+                      ImageItem(imageUri = imageUris[page])
                   }
 
-              IconButton(
-                  onClick = onDismiss,
-                  modifier =
-                      Modifier.align(Alignment.TopEnd)
-                          .size(60.dp)
-                          .testTag("fullscreenImagePopUpCloseButton")) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = "Close",
-                        tint = Color.White)
+
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Box(modifier = Modifier.padding(horizontal = 15.dp)) {
+                  // Like Button
+                  IconButton(
+                      onClick = { /*onLike(imageUris[pagerState.currentPage])*/},
+                      modifier =
+                          Modifier.size(60.dp)
+                              .clip(CircleShape)
+                              .background(ColorPalette.INTERACTION_COLOR_DARK)) {
+                        Icon(
+                            imageVector = Icons.Filled.ThumbUp,
+                            contentDescription = "Like",
+                            tint = Color.White)
+                      }
+                }
+
+                  Box(modifier = Modifier.padding(horizontal = 15.dp)) {
+                  // Dislike Button
+                  IconButton(
+                      onClick = { /*onDislike(imageUris[pagerState.currentPage])*/},
+                      modifier = Modifier.size(60.dp).clip(CircleShape).background(Color.White)) {
+                        Icon(
+                            imageVector = Icons.Filled.ThumbDown,
+                            contentDescription = "Dislike",
+                            tint = ColorPalette.INTERACTION_COLOR_DARK)
+                      }
+                }
+              }
+
+              Column(
+                  horizontalAlignment = Alignment.CenterHorizontally,
+                  verticalArrangement = Arrangement.spacedBy(5.dp),
+                  modifier = Modifier.padding(vertical = 25.dp)) {
+                    Text("Uploaded by @user", color = Color.White)
+                    Text("The dd/MM/yyyy", color = Color.White)
+                    Text("#xxx users liked this picture", color = Color.White)
+                    Text("#xxx users disliked this picture", color = Color.White)
                   }
-            }
+        }
       }
 }
 
