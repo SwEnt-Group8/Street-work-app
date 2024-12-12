988ee2450257c820631d31b4f0e90d594b8cbb65
Arthur Muster <arthur@must-r.com>
2024-12-12 22:53:06 +0100

    feat(test): add settings navigation test

--- app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
@@ -29,12 +29,14 @@ import com.android.streetworkapp.ui.navigation.NavigationActions
 import com.android.streetworkapp.ui.navigation.Route
 import com.android.streetworkapp.utils.GoogleAuthService
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.RETURNS_DEFAULTS
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.mockito.kotlin.whenever
 
 @RunWith(AndroidJUnit4::class)
 class SettingsTest {
@@ -164,6 +166,44 @@ class SettingsTest {
 
     // Click the logout button
     composeTestRule.onNodeWithTag("LogOutButton").performClick()
+    composeTestRule.waitForIdle()
+
+    // Verify that the navigation action to the AUTH screen was called
+    verify(navigationActions).navigateTo(Route.AUTH)
+  }
+
+  @Test
+  fun testDeleteAccountButtonNavigatesToAuthScreen() {
+    val showSettingDialog = mutableStateOf(false)
+    var context: Context? = null
+    val alice = User("uid-alice", "Alice", "alice@gmail.com", 42, emptyList(), "")
+    userViewModel.setCurrentUser(alice)
+    val firebaseAuth = mock(FirebaseAuth::class.java, RETURNS_DEFAULTS)
+    val currentUser = mock(FirebaseUser::class.java)
+    whenever(firebaseAuth.currentUser).thenReturn(currentUser)
+
+    composeTestRule.setContent {
+      val authService =
+          GoogleAuthService(
+              "abc", mock(FirebaseAuth::class.java, RETURNS_DEFAULTS), LocalContext.current)
+      whenever(authService.getCurrentUser()).thenReturn(currentUser)
+      SettingsContent(
+          navigationActions,
+          userViewModel,
+          parkViewModel,
+          eventViewModel,
+          progressionViewModel,
+          workoutViewModel,
+          preferencesViewModel,
+          authService,
+          showSettingDialog)
+      context = LocalContext.current
+    }
+
+    // Click the logout button
+    composeTestRule.onNodeWithTag("DeleteAccountButton").performClick()
+    composeTestRule.waitForIdle()
+    composeTestRule.onNodeWithTag("deleteAccountDialogConfirmButton").performClick()
     composeTestRule.waitForIdle()
 
     // Verify that the navigation action to the AUTH screen was called
