fc2397a6faced1656b5278e3dec41236da1588a8
Kemin <kemin.zhengepfl.ch>
2024-12-12 22:45:34 +0100

    fix(test): KTFMT format

--- app/src/androidTest/java/com/android/streetworkapp/ui/event/EventOverviewTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/event/EventOverviewTest.kt
@@ -82,7 +82,7 @@ class EventOverviewTest {
             occupancy = 5,
             events = emptyList())
 
-    participant = User("123", "test", "test", 0,emptyList(), "test", emptyList())
+    participant = User("123", "test", "test", 0, emptyList(), "test", emptyList())
     joiner = participant.copy(uid = "joiner")
     owner = participant.copy(uid = "124", username = "owner")
 
