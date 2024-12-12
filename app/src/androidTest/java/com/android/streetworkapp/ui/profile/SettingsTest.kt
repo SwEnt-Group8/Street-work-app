bc155e9800b998ff0daa6af7c3e9558393c08c44
Arthur Muster <arthur@must-r.com>
2024-12-12 23:31:35 +0100

    feat(test): add account deletion navigation test

--- app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/profile/SettingsTest.kt
@@ -35,6 +35,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.RETURNS_DEFAULTS
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.never
 import org.mockito.Mockito.verify
 import org.mockito.kotlin.whenever
 
@@ -209,5 +210,43 @@ class SettingsTest {
 
     // Verify that the navigation action to the AUTH screen was called
     verify(navigationActions).navigateTo(Route.AUTH)
+  }
+
+  @Test
+  fun testDeleteAccountButtonFail() {
+    val showSettingDialog = mutableStateOf(false)
+    var context: Context? = null
+    val alice = User("uid-alice", "Alice", "alice@gmail.com", 42, emptyList(), "")
+    userViewModel.setCurrentUser(alice)
+    val firebaseAuth = mock(FirebaseAuth::class.java, RETURNS_DEFAULTS)
+    val currentUser = mock(FirebaseUser::class.java)
+    whenever(firebaseAuth.currentUser).thenReturn(currentUser)
+
+    composeTestRule.setContent {
+      val authService =
+          GoogleAuthService(
+              "abc", mock(FirebaseAuth::class.java, RETURNS_DEFAULTS), LocalContext.current)
+      whenever(authService.getCurrentUser()).thenReturn(null)
+      SettingsContent(
+          navigationActions,
+          userViewModel,
+          parkViewModel,
+          eventViewModel,
+          progressionViewModel,
+          workoutViewModel,
+          preferencesViewModel,
+          authService,
+          showSettingDialog)
+      context = LocalContext.current
+    }
+
+    // Click the logout button
+    composeTestRule.onNodeWithTag("DeleteAccountButton").performClick()
+    composeTestRule.waitForIdle()
+    composeTestRule.onNodeWithTag("deleteAccountDialogConfirmButton").performClick()
+    composeTestRule.waitForIdle()
+
+    // Verify that the navigation action to the AUTH screen was called
+    verify(navigationActions, never()).navigateTo(Route.AUTH)
   }
 }
