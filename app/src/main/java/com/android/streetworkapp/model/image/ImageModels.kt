4dd42631bff364634582136354455cd8e5b75651
Alex <alexandre.noren@hotmail.com>
2024-12-13 16:02:51 +0100

    feat: add ImageRating data class

--- app/src/main/java/com/android/streetworkapp/model/image/ImageModels.kt
+++ app/src/main/java/com/android/streetworkapp/model/image/ImageModels.kt
@@ -1,6 +1,7 @@
 package com.android.streetworkapp.model.image
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Exclude
 import kotlin.math.log
 import kotlin.math.pow
 
@@ -11,12 +12,21 @@ import kotlin.math.pow
  * @property images A list of ParkImage
  */
 data class ParkImageCollection(
-    val id: String,
-    val images: List<ParkImage>
+    val id: String = "",
+    val images: List<ParkImage> = emptyList()
 ) // note: if images field were to be changed, make sure to match the field name change in
 // ImageRepositoryFirestore::uploadImage
 
-//TODO. define score class with internal function to retrieve
+data class ImageRating(val positiveVotes: Int = 0, val negativeVotes: Int = 0, val positiveVotesUids: List<String> = emptyList(), val negativeVotesUids: List<String> = emptyList()) {
+    /**
+     * Returns a computed value from the [Pair<Int, Int>] of the score.
+     * It's a weighted score slowly increasing for the total number of votes, prioritizing the ratio of positive to negative reviews.
+     */
+    @Exclude //we don't want this to get serialized
+    fun getImageScore(): Double {
+        return log((this.positiveVotes + this.negativeVotes).toDouble().pow(0.4), 2.0)*this.positiveVotes/this.negativeVotes
+    }
+}
 
 /**
  * A data class that represents an image related to a park in the database, uploaded by a user, and
@@ -31,10 +41,10 @@ data class ParkImageCollection(
 // * Note: for the rating I would have used an unsigned int but there isn't a built in serializer
 // for it and I can't be bothered
 data class ParkImage(
-    val imageUrl: String,
-    val userId: String,
-    val username: String,
-    val rating: Pair<Int, Int> = Pair(0, 0),
+    val imageUrl: String = "",
+    val userId: String = "",
+    val username: String = "",
+    val rating: ImageRating = ImageRating(0, 0),
     val uploadDate: Timestamp = Timestamp.now()
 )
 
@@ -43,14 +53,3 @@ enum class VOTE_TYPE(val value: Int) {
   NEGATIVE(1)
 }
 
-object ImageModelsUtils {
-    /**
-     * Returns a computed value from the [Pair<Int, Int>] of the score.
-     * It's a weighted score slowly increasing for the total number of votes, prioritizing the ratio of positive to negative reviews.
-     * @param score The score
-     */
-    fun getImageScore(score: Pair<Int, Int>): Double {
-        val totalVotes = score.first + score.second
-        return log(totalVotes.toDouble().pow(0.4), 2.0)*score.first/score.second
-    }
-}
