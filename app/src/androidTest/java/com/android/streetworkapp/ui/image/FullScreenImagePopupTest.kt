baf90ae04ce70e8bb467f787f1b5257ec30ed532
Alex <alexandre.noren@hotmail.com>
2024-12-16 09:57:34 +0100

    tests: verify button onclicks call correct functions

--- app/src/androidTest/java/com/android/streetworkapp/ui/image/FullScreenImagePopupTest.kt
+++ app/src/androidTest/java/com/android/streetworkapp/ui/image/FullScreenImagePopupTest.kt
@@ -3,21 +3,27 @@ package com.android.streetworkapp.ui.image
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.android.streetworkapp.model.image.ImageRating
 import com.android.streetworkapp.model.image.ImageRepository
 import com.android.streetworkapp.model.image.ImageViewModel
 import com.android.streetworkapp.model.image.ParkImage
+import com.android.streetworkapp.model.image.VOTE_TYPE
 import com.android.streetworkapp.model.park.Park
 import com.android.streetworkapp.model.user.User
 import com.android.streetworkapp.model.user.UserRepository
 import com.android.streetworkapp.model.user.UserViewModel
 import com.google.firebase.Timestamp
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class FullScreenImagePopupTest {
 
@@ -70,41 +76,59 @@ class FullScreenImagePopupTest {
   }
 
   @Test
-  fun `retractVote button is shown if user is in image voting list`() {
-    val userInVotingList = baseUser.copy(uid = baseImage.rating.positiveVotesUids[0])
-    userViewModel.setCurrentUser(userInVotingList)
+  fun `retractVote button is shown if user is in image voting list and call correct function onClick`() =
+      runTest {
+        whenever(imageRepository.retractImageVote(any(), any(), any())).thenReturn(true)
 
-    composeTestRule.setContent {
-      FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
-    }
+        val userInVotingList = baseUser.copy(uid = baseImage.rating.positiveVotesUids[0])
+        userViewModel.setCurrentUser(userInVotingList)
 
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag("retractVoteButton").assertIsDisplayed()
-  }
+        composeTestRule.setContent {
+          FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
+        }
 
-  @Test
-  fun `deleteImage button is shown if user is owner of image`() {
-    val userOwnerOfImage = baseUser.copy(uid = baseImage.userId)
-    userViewModel.setCurrentUser(userOwnerOfImage)
-
-    composeTestRule.setContent {
-      FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
-    }
-
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag("deleteImageButton").assertIsDisplayed()
-  }
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithTag("retractVoteButton").assertIsDisplayed().performClick()
+        verify(imageRepository)
+            .retractImageVote(park.imagesCollectionId, baseImage.imageUrl, userInVotingList.uid)
+      }
 
   @Test
-  fun `voteImage buttons are shown if user is neither owner of image nor in image voting list`() {
-    userViewModel.setCurrentUser(baseUser)
+  fun `deleteImage button is shown if user is owner of image and call correct function onClick`() =
+      runTest {
+        whenever(imageRepository.deleteImage(any(), any())).thenReturn(true)
 
-    composeTestRule.setContent {
-      FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
-    }
+        val userOwnerOfImage = baseUser.copy(uid = baseImage.userId)
+        userViewModel.setCurrentUser(userOwnerOfImage)
 
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag("likeImageButton").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("dislikeImageButton").assertIsDisplayed()
-  }
+        composeTestRule.setContent {
+          FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
+        }
+
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithTag("deleteImageButton").assertIsDisplayed().performClick()
+        verify(imageRepository).deleteImage(park.imagesCollectionId, baseImage.imageUrl)
+      }
+
+  @Test
+  fun `voteImage buttons are shown if user is neither owner of image nor in image voting list and call correct functions onClick`() =
+      runTest {
+        whenever(imageRepository.imageVote(any(), any(), any(), any())).thenReturn(true)
+        userViewModel.setCurrentUser(baseUser)
+
+        composeTestRule.setContent {
+          FullScreenImagePopup(listOf(baseImage), park, userViewModel, imageViewModel) {}
+        }
+
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithTag("likeImageButton").assertIsDisplayed().performClick()
+        verify(imageRepository)
+            .imageVote(
+                park.imagesCollectionId, baseImage.imageUrl, baseUser.uid, VOTE_TYPE.POSITIVE)
+
+        composeTestRule.onNodeWithTag("dislikeImageButton").assertIsDisplayed().performClick()
+        verify(imageRepository)
+            .imageVote(
+                park.imagesCollectionId, baseImage.imageUrl, baseUser.uid, VOTE_TYPE.NEGATIVE)
+      }
 }
