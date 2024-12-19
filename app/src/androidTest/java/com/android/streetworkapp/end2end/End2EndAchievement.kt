7d7ee45238d359cb0cf8e90459d10b23ee0efd06
Alvaro Moya <145993917+alvaro1080@users.noreply.github.com>
2024-12-19 15:12:02 +0100

    feat: setup file for end2end based on achievements

---
+++ app/src/androidTest/java/com/android/streetworkapp/end2end/End2EndAchievement.kt
@@ -0,0 +1,19 @@
+package com.android.streetworkapp.end2end
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Rule
+
+class End2EndAchievement {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    // grant the permission to access location (remove the window for permission)
+    @get:Rule
+    val permissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+
+}
